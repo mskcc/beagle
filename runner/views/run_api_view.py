988e2394800ed9aeb254a42c83fa1c6250ff05c6
Ivkovic <sinisa.ivkovic@gmail.com>
2019-10-31 17:24:07 -0400

    operator, submiter and RunAPI finished

--- runner/views/run_api_view.py
+++ runner/views/run_api_view.py
@@ -6,8 +6,11 @@ from django.conf import settings
 from django.db.models import Prefetch
 from rest_framework import status
 from rest_framework import mixins
+from runner.tasks import create_run_task
 from runner.models import Run, Port, Pipeline, RunStatus
-from runner.serializers import RunSerializer, APIRunCreateSerializer, UpdateRunSerializer, RunStatusUpdateSerializer
+from runner.serializers import RunSerializer, APIRunCreateSerializer, UpdateRunSerializer, RunStatusUpdateSerializer, TempoOperatorTestSerializer
+from runner.operator.tempo_operator.tempo_operator import TempoOperator
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -27,5 +30,22 @@ class RunApiViewSet(mixins.ListModelMixin,
         if serializer.is_valid():
             run = serializer.save()
             response = RunSerializer(run)
+            create_run_task.delay(run.id, request.data['inputs'])
             return Response(response.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TempoOperatorViewSet(GenericAPIView):
+    serializer_class = TempoOperatorTestSerializer
+
+    def post(self, request):
+        request_id = request.data['request_id']
+        tempo_operator = TempoOperator(request_id)
+        jobs = tempo_operator.get_jobs()
+        result = []
+        for job in jobs:
+            if job.is_valid():
+                run = job.save()
+                result.append(run)
+        response = RunSerializer(result, many=True)
+        return Response(response.data, status=status.HTTP_200_OK)
