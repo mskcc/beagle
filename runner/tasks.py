348441dfc80afb7d37e91f62677274c5c7d9f911
Ivkovic <sinisa.ivkovic@gmail.com>
2019-10-31 17:38:34 -0400

    add missing file

---
+++ runner/tasks.py
@@ -0,0 +1,60 @@
+import logging
+import datetime
+from .models import Run, RunStatus, Port, PortType
+from celery import shared_task
+import runner.run.run_creator
+from runner.pipeline.pipeline_resolver import CWLResolver
+
+
+logger = logging.getLogger(__name__)
+
+
+@shared_task
+def create_run_task(run_id, inputs):
+    logger.info("Creating and validating Run")
+    try:
+        run = Run.objects.get(id=run_id)
+    except Run.DoesNotExist:
+        raise Exception("Failed to create a run")
+    cwl_resolver = CWLResolver(run.app.github, run.app.entrypoint, run.app.version)
+    resolved_dict = cwl_resolver.resolve()
+    task = runner.run.run_creator.Run(resolved_dict, inputs)
+    for input in task.inputs:
+        port = Port(run=run, name=input.id, port_type=input.type, schema=input.schema,
+                    secondary_files=input.secondary_files, db_value=inputs[input.id], value=input.value)
+        port.save()
+    for output in task.outputs:
+        port = Port(run=run, name=output.id, port_type=output.type, schema=output.schema,
+                    secondary_files=output.secondary_files, db_value=output.value)
+        port.save()
+    run.status = RunStatus.READY
+    submit_job.delay(run_id)
+    logger.info("Run created")
+
+
+@shared_task
+def submit_job(run_id):
+    try:
+        run = Run.objects.get(id=run_id)
+    except Run.DoesNotExist:
+        raise Exception("Failed to submit a run")
+    app ={
+        "github": {
+            "repository": run.app.github,
+            "entrypoint": run.app.entrypoint,
+            "version": run.app.version
+        }
+    }
+    inputs = dict()
+    for port in run.port_set.filter(port_type=PortType.INPUT).all():
+        inputs[port.name] = port.value
+    job = {
+        'app' : app,
+        'inputs': inputs
+    }
+    logger.info("Ready for submittion")
+    print(job)
+
+
+def operator_job(request_id, pipeline_type):
+    pass
