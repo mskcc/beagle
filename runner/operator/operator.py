9fcb4fd7a02eb925ee122e5bc52596ac2b70a621
(Christopher) Allan Bolipata <bolipatc@mskcc.org>
2019-11-01 14:51:01 -0400

    Changed the way file metadata was being pulled, more inline with django

--- runner/operator/operator.py
+++ runner/operator/operator.py
@@ -1,4 +1,5 @@
 from file_system.models import File, FileMetadata
+from django.db.models import Prefetch
 
 
 class Operator(object):
@@ -6,8 +7,10 @@ class Operator(object):
     def __init__(self, request_id):
         self.pipeline_id = self.get_pipeline_id()
         self.request_id = request_id
-        self.files = File.objects
-        self.filemetadata = FileMetadata.objects
+        self.files = File.objects.prefetch_related(
+            Prefetch('filemetadata_set', queryset=
+            FileMetadata.objects.select_related('file').order_by('-created_date'))).\
+            order_by('file_name')
 
     def get_pipeline_id(self):
         '''
