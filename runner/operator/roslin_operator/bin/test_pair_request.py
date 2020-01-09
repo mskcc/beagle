57fc44a2163378a8319696a06ec1d3227130fec0
Stephen Kelly <stevekm@users.noreply.github.com>
2020-01-09 14:44:17 -0500

    add pairing test for multiple requests in db

--- runner/operator/roslin_operator/bin/test_pair_request.py
+++ runner/operator/roslin_operator/bin/test_pair_request.py
@@ -217,104 +217,58 @@ class TestPairRequest(TestCase):
         This time also load File entries from another request to make sure they do not confound the pairing
         """
         # Load fixtures
-        test_files_fixture = os.path.join(settings.TEST_FIXTURE_DIR, "10075_D.file.json")
-        call_command('loaddata', test_files_fixture, verbosity=0)
-        test_files_fixture = os.path.join(settings.TEST_FIXTURE_DIR, "10075_D.filemetadata.json")
-        call_command('loaddata', test_files_fixture, verbosity=0)
-
-        # create some more entries
-        import_filegroup_id = settings.IMPORT_FILE_GROUP
-        filegroup_instance = FileGroup.objects.get(id = import_filegroup_id)
-        filetype_instance = FileType.objects.get(ext = "fastq")
-
-        file1_R1_instance = File.objects.create(
-        file_name = "file1.R1.fastq.gz",
-        path = "/path/to/file1.R1.fastq.gz",
-        file_group = filegroup_instance,
-        file_type = filetype_instance
-        )
-        file1_R2_instance = File.objects.create(
-        file_name = "file1.R2.fastq.gz",
-        path = "/path/to/file1.R2.fastq.gz",
-        file_group = filegroup_instance,
-        file_type = filetype_instance
-        )
-        file2_R1_instance = File.objects.create(
-        file_name = "file2.R1.fastq.gz",
-        path = "/path/to/file2.R1.fastq.gz",
-        file_group = filegroup_instance,
-        file_type = filetype_instance
-        )
-        file2_R2_instance = File.objects.create(
-        file_name = "file2.R2.fastq.gz",
-        path = "/path/to/file2.R2.fastq.gz",
-        file_group = filegroup_instance,
-        file_type = filetype_instance
-        )
-
-        base_metadata = {
-        "R": "R1",
-        "sex": "F",
-        "runId": "Run1",
-        "recipe": "IMPACT468",
-        "baitSet": "IMPACT468_BAITS",
-        "runMode": "HiSeq High Output",
-        "species": "Human",
-        "sampleId": "10075_D_5",
-        "barcodeId": "DUAL_IDT_LIB_267",
-        "libraryId": "10075_D_5_1_1_1",
-        "patientId": "C-8VK0V7",
-        "requestId": "10075_D",
-        "flowCellId": "HFTCNBBXY",
-        "readLength": "101/8/8/101",
-        "sampleName": "C-8VK0V7-R001-d",
-        "captureName": "Pool-09483_R-10075_D-Tube7_1",
-        "igocomplete": true,
-        "labHeadName": "John Smith",
-        "barcodeIndex": "GTATTGGC-TTGTCGGT",
-        "labHeadEmail": "email@internet.com",
-        "oncoTreeCode": "MEL",
-        "preservation": "Frozen",
-        "sampleOrigin": "Tissue",
-        "specimenType": "Resection",
-        "flowCellLanes": [
-            3,
-            4
-        ],
-        "libraryVolume": 35.0,
-        "pooledNormals": [
-            "/ifs/archive/GCL/hiseq/FASTQ/JAX_0397_BHCYYWBBXY/Project_POOLEDNORMALS/Sample_FFPEPOOLEDNORMAL_IGO_IMPACT468_GTGAAGTG/FFPEPOOLEDNORMAL_IGO_IMPACT468_GTGAAGTG_S5_R1_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/JAX_0397_BHCYYWBBXY/Project_POOLEDNORMALS/Sample_FFPEPOOLEDNORMAL_IGO_IMPACT468_GTGAAGTG/FFPEPOOLEDNORMAL_IGO_IMPACT468_GTGAAGTG_S5_R2_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/JAX_0397_BHCYYWBBXY/Project_POOLEDNORMALS/Sample_FROZENPOOLEDNORMAL_IGO_HemePACT_v4_GTTACGCA/FROZENPOOLEDNORMAL_IGO_HemePACT_v4_GTTACGCA_S27_R1_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/JAX_0397_BHCYYWBBXY/Project_POOLEDNORMALS/Sample_FROZENPOOLEDNORMAL_IGO_HemePACT_v4_GTTACGCA/FROZENPOOLEDNORMAL_IGO_HemePACT_v4_GTTACGCA_S27_R2_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/JAX_0397_BHCYYWBBXY/Project_POOLEDNORMALS/Sample_FROZENPOOLEDNORMAL_IGO_IMPACT468_GTTACGCA/FROZENPOOLEDNORMAL_IGO_IMPACT468_GTTACGCA_S12_R1_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/JAX_0397_BHCYYWBBXY/Project_POOLEDNORMALS/Sample_FROZENPOOLEDNORMAL_IGO_IMPACT468_GTTACGCA/FROZENPOOLEDNORMAL_IGO_IMPACT468_GTTACGCA_S12_R2_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/PITT_0439_BHFTCNBBXY/Project_POOLEDNORMALS/Sample_FFPEPOOLEDNORMAL_IGO_IMPACT468_CAGTCTTC/FFPEPOOLEDNORMAL_IGO_IMPACT468_CAGTCTTC_S16_R1_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/PITT_0439_BHFTCNBBXY/Project_POOLEDNORMALS/Sample_FFPEPOOLEDNORMAL_IGO_IMPACT468_CAGTCTTC/FFPEPOOLEDNORMAL_IGO_IMPACT468_CAGTCTTC_S16_R2_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/PITT_0439_BHFTCNBBXY/Project_POOLEDNORMALS/Sample_FROZENPOOLEDNORMAL_IGO_HemePACT_v4_CTAACTCG/FROZENPOOLEDNORMAL_IGO_HemePACT_v4_CTAACTCG_S95_R1_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/PITT_0439_BHFTCNBBXY/Project_POOLEDNORMALS/Sample_FROZENPOOLEDNORMAL_IGO_HemePACT_v4_CTAACTCG/FROZENPOOLEDNORMAL_IGO_HemePACT_v4_CTAACTCG_S95_R2_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/PITT_0439_BHFTCNBBXY/Project_POOLEDNORMALS/Sample_FROZENPOOLEDNORMAL_IGO_IMPACT468_CTAACTCG/FROZENPOOLEDNORMAL_IGO_IMPACT468_CTAACTCG_S7_R1_001.fastq.gz",
-            "/ifs/archive/GCL/hiseq/FASTQ/PITT_0439_BHFTCNBBXY/Project_POOLEDNORMALS/Sample_FROZENPOOLEDNORMAL_IGO_IMPACT468_CTAACTCG/FROZENPOOLEDNORMAL_IGO_IMPACT468_CTAACTCG_S7_R2_001.fastq.gz"
-        ],
-        "tumorOrNormal": "Tumor",
-        "captureInputNg": "110.0",
-        "cmoSampleClass": "Local Recurrence",
-        "collectionYear": "",
-        "tissueLocation": "",
-        "dataAnalystName": "",
-        "dataAnalystEmail": "",
-        "externalSampleId": "SK_MEL_1091A_T",
-        "investigatorName": "Jane Doe",
-        "investigatorEmail": "email2@internet.com",
-        "projectManagerName": "",
-        "investigatorSampleId": "SK_MEL_1091A_T",
-        "captureConcentrationNm": "8.333333333333334",
-        "libraryConcentrationNgul": 13.2
-        }
+        call_command('loaddata',
+            os.path.join(settings.TEST_FIXTURE_DIR, "10075_D.file.json"),
+            verbosity=0)
+        call_command('loaddata',
+            os.path.join(settings.TEST_FIXTURE_DIR, "10075_D.filemetadata.json"),
+            verbosity=0)
+        call_command('loaddata',
+            os.path.join(settings.TEST_FIXTURE_DIR, "05257_CB.file.json"),
+            verbosity=0)
+        call_command('loaddata',
+            os.path.join(settings.TEST_FIXTURE_DIR, "05257_CB.filemetadata.json"),
+            verbosity=0)
 
         # check the total number of db entries now
-        files = File.objects.all()
-        filesMetadata = FileMetadata.objects.all()
-        print(len(files))
-        self.assertTrue(len(files) == 14)
-        self.assertTrue(len(filesMetadata) == 0)
+        self.assertTrue(len(File.objects.all()) == 14)
+        self.assertTrue(len(FileMetadata.objects.all()) == 18)
+
+        samples = [
+        {
+        "bait_set": "IMPACT468_BAITS",
+        "patient_id": "C-DRKHP7",
+        "tumor_type": "Tumor",
+        "igo_id": "10075_D_1"
+        }
+        ]
+        pairs = compile_pairs(samples)
+        expected_pairs = {
+        'normal': [{
+            'CN': 'MSKCC',
+            'ID': ['s_C_DRKHP7_N001_d_HCYYWBBXY'],
+            'LB': '10075_D_2',
+            'PL': 'Illumina',
+            'PU': ['HCYYWBBXY'],
+            'R1': ['/ifs/archive/GCL/hiseq/FASTQ/JAX_0397_BHCYYWBBXY/Project_10075_D/Sample_31-N_IGO_10075_D_2/31-N_IGO_10075_D_2_S14_R1_001.fastq.gz'],
+            'R1_bid': [UUID('40a07e9a-2198-40b7-9f7f-7696c9d6429e')],
+            'R2': ['/ifs/archive/GCL/hiseq/FASTQ/JAX_0397_BHCYYWBBXY/Project_10075_D/Sample_31-N_IGO_10075_D_2/31-N_IGO_10075_D_2_S14_R2_001.fastq.gz'],
+            'R2_bid': [UUID('bb7ff922-b741-4df7-ba2a-4f3b8549e8b5')],
+            'SM': 's_C_DRKHP7_N001_d',
+            'bait_set': 'IMPACT468_BAITS',
+            'igo_id': '10075_D_2',
+            'patient_id': 'C-DRKHP7',
+            'request_id': ['10075_D'],
+            'run_date': ['2019-12-12'],
+            'species': 'Human',
+            'specimen_type': 'Blood',
+            'tumor_type': 'Normal'
+            }],
+        'tumor': [{
+            'bait_set': 'IMPACT468_BAITS',
+            'igo_id': '10075_D_1',
+            'patient_id': 'C-DRKHP7',
+            'tumor_type': 'Tumor'
+            }]
+        }
+        self.assertTrue(pairs == expected_pairs)
