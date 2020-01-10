ecc2dd11892a2085fbff448308986ed2e95142ec
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-01-10 16:59:00 -0500

    add requestId in Run tags, and change /v0/run/api in order to be filtered by requestId

--- runner/views/run_api_view.py
+++ runner/views/run_api_view.py
@@ -4,12 +4,13 @@ import base64
 import requests
 import logging
 from django.conf import settings
+from beagle.pagination import time_filter
 from django.db.models import Prefetch
 from rest_framework import status
 from rest_framework import mixins
 from runner.tasks import create_run_task, operator_job
 from runner.models import Run, Port, Pipeline, RunStatus, OperatorErrors
-from runner.serializers import RunSerializer, APIRunCreateSerializer, RequestIdOperatorSerializer, OperatorErrorSerializer
+from runner.serializers import RunSerializerPartial, RunSerializerFull, APIRunCreateSerializer, RequestIdOperatorSerializer, OperatorErrorSerializer
 from runner.operator.tempo_operator.tempo_operator import TempoOperator
 from rest_framework.generics import GenericAPIView
 from runner.pipeline.pipeline_resolver import CWLResolver
@@ -26,13 +27,48 @@ class RunApiViewSet(mixins.ListModelMixin,
     queryset = Run.objects.prefetch_related(Prefetch('port_set', queryset=
     Port.objects.select_related('run'))).order_by('-created_date').all()
 
-    serializer_class = RunSerializer
+    serializer_class = RunSerializerFull
+
+    def list(self, request, *args, **kwargs):
+
+        queryset = Run.objects.prefetch_related(
+            Prefetch('port_set', queryset=Port.objects.select_related('run'))).order_by('-created_date').all()
+        queryset = time_filter(Run, request.query_params)
+        status_param = request.query_params.get('status')
+        if status_param:
+            if status_param not in [s.name for s in RunStatus]:
+                return Response({'details': 'Invalid status value %s: expected values %s' % (status_param, [s.name for s in RunStatus])}, status=status.HTTP_400_BAD_REQUEST)
+            queryset = queryset.filter(status=RunStatus[status_param].value)
+        input_params = request.query_params.get('input')
+        if input_params:
+            inputs = input_params.split(',')
+            for input in inputs:
+                try:
+                    key, val = input.split(':')
+                    query = {"port__value__%s" % key: val}
+                    queryset = queryset.filter(**query).all()
+                except Exception as e:
+                    return Response({'details': 'Query for inputs needs to be in format input:value'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+        request_id = request.query_params.get('requestId')
+        if request_id:
+            queryset = queryset.filter(tags__requestId=request_id).all()
+        page = self.paginate_queryset(queryset)
+        full = request.query_params.get('full')
+        if page is not None:
+            if full:
+                serializer = RunSerializerFull(page, many=True)
+            else:
+                serializer = RunSerializerPartial(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        return Response([], status=status.HTTP_200_OK)
+
 
     def create(self, request, *args, **kwargs):
         serializer = APIRunCreateSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             run = serializer.save()
-            response = RunSerializer(run)
+            response = RunSerializerFull(run)
             # cwl_resolver = CWLResolver(run.app.github, run.app.entrypoint, run.app.version)
             # resolved_dict = cwl_resolver.resolve()
             # task = runner.run.run_creator.Run(resolved_dict, request.data['inputs'])
@@ -49,7 +85,7 @@ class RunApiViewSet(mixins.ListModelMixin,
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class TempoOperatorViewSet(GenericAPIView):
+class OperatorViewSet(GenericAPIView):
     serializer_class = RequestIdOperatorSerializer
 
     logger = logging.getLogger(__name__)
