3dbf1b1e3cbafd51aed6c02e21cd1fe754c52d4c
Stephen Kelly <stevekm@users.noreply.github.com>
2020-02-11 12:59:33 -0500

    start work on passing output metadata from Roslin operator

--- runner/operator/roslin_operator/roslin_operator.py
+++ runner/operator/roslin_operator/roslin_operator.py
@@ -5,7 +5,12 @@ from runner.serializers import APIRunCreateSerializer
 from .construct_roslin_pair import construct_roslin_jobs
 from .bin.pair_request import compile_pairs
 from .bin.make_sample import build_sample
+from pprint import pprint
 
+import beagle_etl.celery
+beagle_etl.celery.app.conf['task_always_eager'] = True
+
+pprint("roslin_operator module loaded...", stream = open("debug.log", "a"))
 
 class RoslinOperator(Operator):
 
@@ -43,11 +48,25 @@ class RoslinOperator(Operator):
         roslin_inputs, error_samples = construct_roslin_jobs(samples)
         number_of_inputs = len(roslin_inputs)
 
-        for i, job in enumerate(roslin_inputs):
+        for i, job_items in enumerate(roslin_inputs):
+            job = job_items[0]
+            job_metadata = job_items[1]
             tumor_sample_name = job['pair'][0]['ID']
             normal_sample_name = job['pair'][1]['ID']
             name = "ROSLIN %s, %i of %i" % (self.request_id, i + 1, number_of_inputs)
-            roslin_jobs.append((APIRunCreateSerializer(
-                data={'app': self.get_pipeline_id(), 'inputs': roslin_inputs, 'name': name,
-                      'tags': {'requestId': self.request_id}}), job))
+            data = {
+                    'app': self.get_pipeline_id(),
+                    'inputs': roslin_inputs,
+                    'name': name,
+                    'tags': {'requestId': self.request_id},
+                    'output_metadata': {
+                        'assay': job_metadata['assay'],
+                        'request_id': job_metadata['request_id'],
+                        'tumor_igo_id': job_metadata['tumor']['igo_id'],
+                        'tumor_patient_id': job_metadata['tumor']['patient_id'],
+                        'normal_igo_id': job_metadata['normal']['igo_id'],
+                        'normal_patient_id': job_metadata['normal']['patient_id'],
+                        }
+                    }
+            roslin_jobs.append((APIRunCreateSerializer(data = data), job))
         return roslin_jobs
