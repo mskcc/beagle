765b9630e1a4d766ae55953b5ee275d28b54053c
Stephen Kelly <stevekm@users.noreply.github.com>
2020-02-18 13:54:56 -0500

    add RoslinQCOperator to get by classname for Operator Factory

--- runner/tests/operator/roslin_qc_operator/test_roslin_qc_operator.py
+++ runner/tests/operator/roslin_qc_operator/test_roslin_qc_operator.py
@@ -5,19 +5,28 @@ from django.test import TestCase
 from runner.operator.operator_factory import OperatorFactory
 from runner.operator.roslin_qc_operator.roslin_qc_operator import RoslinQcOperator
 class TestOperatorFactory(TestCase):
-    def test_true(self):
-        self.assertTrue(True)
-
     def test_operator_factory_roslin_qc1(self):
         """
         Test that a Roslin QC operator instance can be created
         """
         pipeline_type = "roslin-qc"
-        request_id = "bar"
+        request_id = "foo"
         operator = OperatorFactory.factory(pipeline_type, request_id)
         self.assertTrue(isinstance(operator, RoslinQcOperator))
         self.assertTrue( operator.pipeline_id == "replace-this-with-pipeline-id")
         self.assertTrue( operator.operator == "roslin-qc")
-        self.assertTrue( operator.request_id == "bar")
+        self.assertTrue( operator.request_id == "foo")
+        self.assertTrue( operator._jobs == [])
+        self.assertTrue( len(operator.files) == 0)
+
+    def test_operator_factory_get_by_class_name(self):
+        """
+        Test that you can get the RoslinQC operator by name
+        """
+        operator = OperatorFactory.get_by_class_name("RoslinQcOperator", "foo")
+        self.assertTrue(isinstance(operator, RoslinQcOperator))
+        self.assertTrue( operator.pipeline_id == "replace-this-with-pipeline-id")
+        self.assertTrue( operator.operator == "roslin-qc")
+        self.assertTrue( operator.request_id == "foo")
         self.assertTrue( operator._jobs == [])
         self.assertTrue( len(operator.files) == 0)
