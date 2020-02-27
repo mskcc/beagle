99a7c086e5e4a140992fd475aaad0da03f961391
Nikhil <nikhilkumar516@gmail.com>
2020-02-27 14:56:18 -0500

    Added copy outputs operator

---
+++ runner/operator/copy_outputs_operator/copy_outputs_operator.py
@@ -0,0 +1,17 @@
+import uuid
+from rest_framework import serializers
+from runner.operator.operator import Operator
+from runner.serializers import APIRunCreateSerializer
+from .copy_outputs_operator import construct_copy_outputs_input
+
+
+
+class CopyOutputsOperator(Operator):
+    def get_jobs(self):
+        run_ids = self.run_ids
+        input_json = construct_copy_outputs_input(run_ids)
+        number_of_runs = len(run_ids)
+        name = "ROSLIN COPY OUTPUTS %s runs [%s,..] " % (number_of_runs,run_ids[0])
+        copy_outputs_job_data = {'app': self.get_pipeline_id(), 'inputs': input_json, 'name': name}
+        copy_outputs_job = (APIRunCreateSerializer(data=copy_outputs_job_data, input_json))
+        return copy_outputs_job
