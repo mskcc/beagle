915e0556c84e6599171c24a6e1d7a3460ba9e0a9
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-02-28 09:59:45 -0500

    add pi and pi_email in runparams

--- runner/operator/roslin_operator/construct_roslin_pair.py
+++ runner/operator/roslin_operator/construct_roslin_pair.py
@@ -70,7 +70,7 @@ def construct_roslin_jobs(samples):
         job['assay'] = assay
         job['pi'] = pi
         job['pi_email'] = pi_email
-        references = convert_references(project_id, assay)
+        references = convert_references(project_id, assay, pi, pi_email)
         job.update(references)
         roslin_jobs.append(job)
     return roslin_jobs, error_samples
@@ -144,7 +144,7 @@ def get_complex_tn(assay):
     return 0.2
 
 
-def convert_references(project_id, assay):
+def convert_references(project_id, assay, pi, pi_emails):
     roslin_resources = load_references()
     request_files = roslin_resources["request_files"]
     intervals = get_baits_and_targets(assay, roslin_resources)
@@ -212,7 +212,9 @@ def convert_references(project_id, assay):
         "complex_nn": complex_nn,
         "complex_tn": complex_tn,
         "scripts_bin": "/usr/bin",
-        "gatk_jar_path": "/usr/bin/gatk.jar"
+        "gatk_jar_path": "/usr/bin/gatk.jar",
+        "pi": pi,
+        "pi_email": pi_email
     }
     out_dict.update({"runparams": params})
     return out_dict
