776c6f1873cf3c98786e00b0eafc5bd22fbe802e
Allan Bolipata <bolipatc@mskcc.org>
2020-03-03 15:11:11 -0500

    Updated tests

--- runner/tests/operator/roslin_operator/test_pair_request.py
+++ runner/tests/operator/roslin_operator/test_pair_request.py
@@ -84,21 +84,25 @@ class TestPairRequest(TestCase):
         {
         "patient_id": "C-W86LMR",
         "bait_set": "IMPACT468_BAITS",
+        "run_id": ["JAX_0397"],
+        "preservation_type": ["EDTA-Streck"],
         "tumor_type": "Normal"
         },
         {
         "patient_id": "C-W86LMR",
         "bait_set": "IMPACT468_BAITS",
+        "run_id": ["JAX_0397"],
+        "preservation_type": ["EDTA-Streck"],
         "tumor_type": "Tumor"
         }
         ]
         pairs = compile_pairs(samples)
         expected_pairs = {
         'tumor': [
-        {'patient_id': 'C-W86LMR', 'bait_set': 'IMPACT468_BAITS', 'tumor_type': 'Tumor'}
+        {'patient_id': 'C-W86LMR', 'bait_set': 'IMPACT468_BAITS', 'tumor_type': 'Tumor', "run_id": ["JAX_0397"], "preservation_type": ["EDTA-Streck"]}
         ],
         'normal': [
-        {'patient_id': 'C-W86LMR', 'bait_set': 'IMPACT468_BAITS', 'tumor_type': 'Normal'}
+        {'patient_id': 'C-W86LMR', 'bait_set': 'IMPACT468_BAITS', 'tumor_type': 'Normal', "run_id": ["JAX_0397"], "preservation_type": ["EDTA-Streck"]}
         ]
         }
         self.assertTrue(pairs == expected_pairs)
@@ -111,36 +115,46 @@ class TestPairRequest(TestCase):
         {
         "bait_set": "IMPACT468_BAITS",
         "patient_id": "C-DRKHP7",
+        "run_id": ["JAX_0397"],
+        "preservation_type": ["EDTA-Streck"],
         "tumor_type": "Normal"
         },
         {
         "bait_set": "IMPACT468_BAITS",
         "patient_id": "C-8VK0V7",
+        "run_id": ["JAX_0397"],
+        "preservation_type": ["EDTA-Streck"],
         "tumor_type": "Normal"
         },
         {
         "bait_set": "IMPACT468_BAITS",
         "patient_id": "C-DRKHP7",
+        "run_id": ["JAX_0397"],
+        "preservation_type": ["EDTA-Streck"],
         "tumor_type": "Tumor"
         },
         {
         "bait_set": "IMPACT468_BAITS",
         "patient_id": "C-8VK0V7",
+        "run_id": ["JAX_0397"],
+        "preservation_type": ["EDTA-Streck"],
         "tumor_type": "Tumor"
         },
         {
         "bait_set": "IMPACT468_BAITS",
         "patient_id": "C-DRKHP7",
+        "run_id": ["JAX_0397"],
+        "preservation_type": ["EDTA-Streck"],
         "tumor_type": "Tumor"
         }
         ]
         pairs = compile_pairs(samples)
         expected_pairs = {
         'tumor': [
-        {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-DRKHP7', 'tumor_type': 'Tumor'}, {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-8VK0V7', 'tumor_type': 'Tumor'}, {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-DRKHP7', 'tumor_type': 'Tumor'}
+        {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-DRKHP7', 'tumor_type': 'Tumor', "run_id": ["JAX_0397"], "preservation_type": ["EDTA-Streck"]}, {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-8VK0V7', 'tumor_type': 'Tumor', "run_id": ["JAX_0397"], "preservation_type": ["EDTA-Streck"]}, {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-DRKHP7', 'tumor_type': 'Tumor', "run_id": ["JAX_0397"], "preservation_type": ["EDTA-Streck"]}
         ],
         'normal': [
-        {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-DRKHP7', 'tumor_type': 'Normal'}, {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-8VK0V7', 'tumor_type': 'Normal'}, {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-DRKHP7', 'tumor_type': 'Normal'}
+        {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-DRKHP7', 'tumor_type': 'Normal', "run_id": ["JAX_0397"], "preservation_type": ["EDTA-Streck"]}, {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-8VK0V7', 'tumor_type': 'Normal', "run_id": ["JAX_0397"], "preservation_type": ["EDTA-Streck"]}, {'bait_set': 'IMPACT468_BAITS', 'patient_id': 'C-DRKHP7', 'tumor_type': 'Normal', "run_id": ["JAX_0397"], "preservation_type": ["EDTA-Streck"]}
         ]
         }
         self.assertTrue(pairs == expected_pairs)
@@ -153,6 +167,8 @@ class TestPairRequest(TestCase):
         {
         "bait_set": "IMPACT468_BAITS",
         "patient_id": "C-DRKHP7",
+        "run_id": ["JAX_0397"],
+        "preservation_type": ["EDTA-Streck"],
         "tumor_type": "Normal"
         }
         ]
@@ -166,7 +182,8 @@ class TestPairRequest(TestCase):
         Test that the appropriate Normal sample is found from the other samples in the same request
         missing normal for sample 10075_D_1; querying patient C-DRKHP7
         """
-        # Load fixtures
+        # Load fixtures:w
+
         test_files_fixture = os.path.join(settings.TEST_FIXTURE_DIR, "10075_D.file.json")
         call_command('loaddata', test_files_fixture, verbosity=0)
         test_files_fixture = os.path.join(settings.TEST_FIXTURE_DIR, "10075_D.filemetadata.json")
@@ -176,6 +193,8 @@ class TestPairRequest(TestCase):
         {
         "bait_set": "IMPACT468_BAITS",
         "patient_id": "C-DRKHP7",
+        "run_id": ["JAX_0397"],
+        "preservation_type": ["EDTA-Streck"],
         "tumor_type": "Tumor",
         "igo_id": "10075_D_1"
         }
@@ -245,6 +264,8 @@ class TestPairRequest(TestCase):
         "bait_set": "IMPACT468_BAITS",
         "patient_id": "C-DRKHP7",
         "tumor_type": "Tumor",
+        'run_id': ['JAX_0397'],
+        "preservation_type": ["EDTA-Streck"],
         "igo_id": "10075_D_1"
         }
         ]
@@ -266,6 +287,7 @@ class TestPairRequest(TestCase):
             'patient_id': 'C-DRKHP7',
             'request_id': '10075_D',
             'run_id': ['JAX_0397'],
+            "preservation_type": ["EDTA-Streck"],
             'run_date': ['2019-12-12'],
             'species': 'Human',
             'specimen_type': 'Blood',
@@ -276,6 +298,8 @@ class TestPairRequest(TestCase):
             'bait_set': 'IMPACT468_BAITS',
             'igo_id': '10075_D_1',
             'patient_id': 'C-DRKHP7',
+            'run_id': ['JAX_0397'],
+            "preservation_type": ["EDTA-Streck"],
             'tumor_type': 'Tumor'
             }]
         }
@@ -314,6 +338,8 @@ class TestPairRequest(TestCase):
         "bait_set": "IMPACT468_BAITS",
         "patient_id": "C-8VK0V7",
         "tumor_type": "Tumor",
+        'run_id': ['JAX_0397'],
+        "preservation_type": ["EDTA-Streck"],
         "igo_id": "10075_D_3_5",
         "request_id": "10075_D_3"
         }
@@ -325,6 +351,8 @@ class TestPairRequest(TestCase):
             {
             'bait_set': 'IMPACT468_BAITS',
             'patient_id': 'C-8VK0V7',
+            'run_id': ['JAX_0397'],
+            "preservation_type": ["EDTA-Streck"],
             'tumor_type': 'Tumor',
             'igo_id': '10075_D_3_5',
             'request_id': '10075_D_3'
@@ -351,6 +379,7 @@ class TestPairRequest(TestCase):
             'R2_bid': [UUID('c71c259a-ebc0-4490-9af1-bc99387a70d7')],
             'request_id': '10075_D_2',
             'run_id': ['JAX_0397'],
+            "preservation_type": ["EDTA-Streck"],
             'pi': 'John Smith',
             'pi_email': 'email@internet.com' }
             ]
@@ -390,7 +419,8 @@ class TestPairRequest(TestCase):
         "tumor_type": "Tumor",
         "igo_id": "10075_D_3_5",
         "request_id": "10075_D_3",
-        'run_id': ['JAX_0397']
+        'run_id': ['JAX_0397'],
+        "preservation_type": ["EDTA-Streck"],
         }
         ]
 
@@ -404,6 +434,7 @@ class TestPairRequest(TestCase):
             'igo_id': '10075_D_3_5',
             'request_id': '10075_D_3',
             'run_id': ['JAX_0397']
+            "preservation_type": ["EDTA-Streck"]
             }
         ],
         'normal': [
@@ -427,6 +458,7 @@ class TestPairRequest(TestCase):
             'R2_bid': [UUID('f0d9a1e1-9414-42df-a749-08776732ee04')],
             'request_id': '10075_D_4',
             'run_id': ['JAX_0397'],
+            "preservation_type": ["EDTA-Streck"],
             'pi': 'John Smith', 'pi_email': 'email@internet.com'}
             ]
         }
@@ -473,6 +505,7 @@ class TestPairRequest(TestCase):
         'patient_id': 'DU874145',
         'request_id': 'juno_roslin_demo1',
         'run_id': ['JAX_0397'],
+        "preservation_type": ["EDTA-Streck"],
         'run_date': ['2019-12-17'],
         'species': 'Human',
         'specimen_type': 'Resection',
@@ -480,8 +513,8 @@ class TestPairRequest(TestCase):
         pairs = compile_pairs(samples)
 
         expected_pairs = {
-        'tumor': [{'CN': 'MSKCC', 'ID': ['s_juno_roslin_demo1_5_HFTCNBBXY_GTATTGGC-TTGTCGGT'], 'LB': 'juno_roslin_demo1_5_1_1_1', 'PL': 'Illumina', 'PU': ['HFTCNBBXY_GTATTGGC-TTGTCGGT'], 'R1': ['/juno/work/ci/roslin-pipelines/variant/2.6.0/workspace/test_data/examples/data/fastq/DU874145-T/DU874145-T_IGO_00000_TEST_L001_R1_001.fastq.gz'], 'R1_bid': [UUID('d2d8ed36-d8f4-4e93-b038-d38328fad021')], 'R2': ['/juno/work/ci/roslin-pipelines/variant/2.6.0/workspace/test_data/examples/data/fastq/DU874145-T/DU874145-T_IGO_00000_TEST_L001_R2_001.fastq.gz'], 'R2_bid': [UUID('2f77f3ac-ab25-4a02-90bd-86542401ac89')], 'SM': 's_juno_roslin_demo1_5', 'bait_set': 'IMPACT468_BAITS', 'igo_id': 's_juno_roslin_demo1_5', 'patient_id': 'DU874145', 'request_id': 'juno_roslin_demo1', 'run_id': ['JAX_0397'], 'run_date': ['2019-12-17'], 'species': 'Human', 'specimen_type': 'Resection', 'tumor_type': 'Tumor'}],
-        'normal': [{'CN': 'MSKCC', 'ID': ['s_juno_roslin_demo1_3_HCYYWBBXY'], 'LB': 'juno_roslin_demo1_3', 'PL': 'Illumina', 'PU': ['HCYYWBBXY'], 'R1': ['/juno/work/ci/roslin-pipelines/variant/2.6.0/workspace/test_data/examples/data/fastq/DU874145-N/DU874145-N_IGO_00000_TEST_L001_R1_001.fastq.gz'], 'R1_bid': [UUID('a46c5e6b-0793-4cd2-b5dd-92b3d71cf1ac')], 'R2': ['/juno/work/ci/roslin-pipelines/variant/2.6.0/workspace/test_data/examples/data/fastq/DU874145-N/DU874145-N_IGO_00000_TEST_L001_R2_001.fastq.gz'], 'R2_bid': [UUID('c71c259a-ebc0-4490-9af1-bc99387a70d7')], 'SM': 's_juno_roslin_demo1_3', 'bait_set': 'IMPACT468_BAITS', 'igo_id': 's_juno_roslin_demo1_3', 'patient_id': 'DU874145', 'request_id': 'juno_roslin_demo1', 'run_id': ['JAX_0397'], 'run_date': ['2019-12-12'], 'species': 'Human', 'specimen_type': 'Blood', 'tumor_type': 'Normal'}]}
+        'tumor': [{'CN': 'MSKCC', 'ID': ['s_juno_roslin_demo1_5_HFTCNBBXY_GTATTGGC-TTGTCGGT'], 'LB': 'juno_roslin_demo1_5_1_1_1', 'PL': 'Illumina', 'PU': ['HFTCNBBXY_GTATTGGC-TTGTCGGT'], 'R1': ['/juno/work/ci/roslin-pipelines/variant/2.6.0/workspace/test_data/examples/data/fastq/DU874145-T/DU874145-T_IGO_00000_TEST_L001_R1_001.fastq.gz'], 'R1_bid': [UUID('d2d8ed36-d8f4-4e93-b038-d38328fad021')], 'R2': ['/juno/work/ci/roslin-pipelines/variant/2.6.0/workspace/test_data/examples/data/fastq/DU874145-T/DU874145-T_IGO_00000_TEST_L001_R2_001.fastq.gz'], 'R2_bid': [UUID('2f77f3ac-ab25-4a02-90bd-86542401ac89')], 'SM': 's_juno_roslin_demo1_5', 'bait_set': 'IMPACT468_BAITS', 'igo_id': 's_juno_roslin_demo1_5', 'patient_id': 'DU874145', 'request_id': 'juno_roslin_demo1', 'run_id': ['JAX_0397'], 'run_date': ['2019-12-17'], 'species': 'Human', 'specimen_type': 'Resection', 'tumor_type': 'Tumor', "preservation_type": ["EDTA-Streck"]}],
+        'normal': [{'CN': 'MSKCC', 'ID': ['s_juno_roslin_demo1_3_HCYYWBBXY'], 'LB': 'juno_roslin_demo1_3', 'PL': 'Illumina', 'PU': ['HCYYWBBXY'], 'R1': ['/juno/work/ci/roslin-pipelines/variant/2.6.0/workspace/test_data/examples/data/fastq/DU874145-N/DU874145-N_IGO_00000_TEST_L001_R1_001.fastq.gz'], 'R1_bid': [UUID('a46c5e6b-0793-4cd2-b5dd-92b3d71cf1ac')], 'R2': ['/juno/work/ci/roslin-pipelines/variant/2.6.0/workspace/test_data/examples/data/fastq/DU874145-N/DU874145-N_IGO_00000_TEST_L001_R2_001.fastq.gz'], 'R2_bid': [UUID('c71c259a-ebc0-4490-9af1-bc99387a70d7')], 'SM': 's_juno_roslin_demo1_3', 'bait_set': 'IMPACT468_BAITS', 'igo_id': 's_juno_roslin_demo1_3', 'patient_id': 'DU874145', 'request_id': 'juno_roslin_demo1', 'run_id': ['JAX_0397'], 'run_date': ['2019-12-12'], 'species': 'Human', 'specimen_type': 'Blood', 'tumor_type': 'Normal', "preservation_type": ["EDTA-Streck"]}]}
 
         print(json.dumps(pairs, cls=UUIDEncoder))
         print(json.dumps(expected_pairs, cls=UUIDEncoder))
