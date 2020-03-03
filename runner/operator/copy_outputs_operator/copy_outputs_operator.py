7bb217472d879e73f63f6f8075d158cde5655009
Nikhil <nikhilkumar516@gmail.com>
2020-03-03 13:32:22 -0500

    A few slight fixes

--- runner/operator/copy_outputs_operator/copy_outputs_operator.py
+++ runner/operator/copy_outputs_operator/copy_outputs_operator.py
@@ -2,7 +2,7 @@ import uuid
 from rest_framework import serializers
 from runner.operator.operator import Operator
 from runner.serializers import APIRunCreateSerializer
-from .copy_outputs_operator import construct_copy_outputs_input
+from .construct_copy_outputs import construct_copy_outputs_input
 
 
 
@@ -13,5 +13,5 @@ class CopyOutputsOperator(Operator):
         number_of_runs = len(run_ids)
         name = "ROSLIN COPY OUTPUTS %s runs [%s,..] " % (number_of_runs,run_ids[0])
         copy_outputs_job_data = {'app': self.get_pipeline_id(), 'inputs': input_json, 'name': name}
-        copy_outputs_job = (APIRunCreateSerializer(data=copy_outputs_job_data, input_json))
+        copy_outputs_job = (APIRunCreateSerializer(data=copy_outputs_job_data), input_json)
         return copy_outputs_job
