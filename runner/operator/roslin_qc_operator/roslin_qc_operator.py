7a799a4f0443866001828d86bcfbeb90a0c2d356
Allan Bolipata <bolipatc@mskcc.org>
2020-03-12 17:29:33 -0400

    PEP8-ified roslin_qc_operator.py

--- runner/operator/roslin_qc_operator/roslin_qc_operator.py
+++ runner/operator/roslin_qc_operator/roslin_qc_operator.py
@@ -1,15 +1,28 @@
-import uuid
+"""
+RoslinQcOperator
+
+Constructs input JSON for the roslin QC pipeline and then
+submits them as runs
+"""
+import logging
 from rest_framework import serializers
 from runner.operator.operator import Operator
 from runner.serializers import APIRunCreateSerializer
 from .construct_roslin_qc_outputs import construct_roslin_qc_input
-import logging
-import json
 LOGGER = logging.getLogger(__name__)
 
 
 class RoslinQcOperator(Operator):
+    """
+    Constructs input JSON for the roslin QC pipeline and then
+    submits them as runs
+    """
     def get_jobs(self):
+        """
+        From self, retrieve relevant run IDs, build the input JSON for
+        the pipeline, and then submit them as jobs through the
+        APIRunCreateSerializer
+        """
         run_ids = self.run_ids
         input_json = construct_roslin_qc_input(run_ids)
         number_of_runs = len(run_ids)
@@ -20,10 +33,10 @@ class RoslinQcOperator(Operator):
                 "project_prefix": input_json['project_prefix'],
                 "number_of_runs": number_of_runs}
         roslin_qc_outputs_job_data = {
-                'app': self.get_pipeline_id(),
-                'inputs': input_json,
-                'name': name,
-                'tags': tags}
+            'app': self.get_pipeline_id(),
+            'inputs': input_json,
+            'name': name,
+            'tags': tags}
         roslin_qc_outputs_job = [(APIRunCreateSerializer(
             data=roslin_qc_outputs_job_data), input_json)]
         return roslin_qc_outputs_job
