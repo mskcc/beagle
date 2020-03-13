bc882df7bf61a4b62fbd00a0802d84bf1483a441
Allan Bolipata <bolipatc@mskcc.org>
2020-03-13 11:22:54 -0400

    Removed tag field names that didn't exist for copy outputs; added run_ids back in t

--- runner/operator/copy_outputs_operator/copy_outputs_operator.py
+++ runner/operator/copy_outputs_operator/copy_outputs_operator.py
@@ -25,10 +25,8 @@ class CopyOutputsOperator(Operator):
         number_of_runs = len(run_ids)
         name = "ROSLIN COPY OUTPUTS %s runs [%s,..] " % (
             number_of_runs, run_ids[0])
-        tags = {"tumor_sample_names": input_json['tumor_sample_names'],
-                "normal_sample_names": input_json['normal_sample_names'],
-                "project_prefix": input_json['project_prefix'],
-                "number_of_runs": number_of_runs}
+        tags = {"number_of_runs": number_of_runs,
+                "run_ids": run_ids}
         copy_outputs_job_data = {
             'app': self.get_pipeline_id(),
             'inputs': input_json,
