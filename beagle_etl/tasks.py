f1f46e10eb9979f31a5f6cd5663005f7f0c2d16d
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-04-02 12:55:06 -0400

    create sample data clinical file and handle failed jobs

--- beagle_etl/tasks.py
+++ beagle_etl/tasks.py
@@ -4,10 +4,12 @@ import datetime
 import traceback
 from celery import shared_task
 from django.db import transaction
+from django.db.models import Prefetch
 from beagle_etl.models import JobStatus, Job
 from beagle_etl.jobs.lims_etl_jobs import TYPES
+from file_system.models import File, FileMetadata
 from notifier.tasks import send_notification
-from notifier.events import ETLImportEvent, ETLJobsLinksEvent, SetCIReviewEvent
+from notifier.events import ETLImportEvent, ETLJobsLinksEvent, SetCIReviewEvent, UploadAttachmentEvent
 
 
 logger = logging.getLogger(__name__)
@@ -99,103 +101,138 @@ class JobObject(object):
         children = func(**self.job.args)
         self.job.children = children or []
 
+    def _generate_ticket_decription(self):
+        samples_completed = set()
+        samples_failed = set()
+        all_jobs = []
+
+        jobs = Job.objects.filter(job_group=self.job.job_group.id).all()
+
+        for job in jobs:
+            if job.run == TYPES['SAMPLE']:
+                if job.status == JobStatus.COMPLETED:
+                    samples_completed.add(job.args['sample_id'])
+                elif job.status == JobStatus.FAILED:
+                    samples_failed.add(job.args['sample_id'])
+            all_jobs.append(str(job.id))
+
+        request_metadata = Job.objects.filter(args__request_id=self.job.args['request_id'], run=TYPES['SAMPLE']).first()
+
+        data_analyst_email = ""
+        data_analyst_name = ""
+        investigator_email = ""
+        investigator_name = ""
+        lab_head_email = ""
+        lab_head_name = ""
+        pi_email = ""
+        project_manager_name = ""
+        recipe = ""
+
+        if request_metadata:
+            metadata = request_metadata.args.get('request_metadata', {})
+            recipe = metadata['recipe']
+            data_analyst_email = metadata['dataAnalystEmail']
+            data_analyst_name = metadata['dataAnalystName']
+            investigator_email = metadata['investigatorEmail']
+            investigator_name = metadata['investigatorName']
+            lab_head_email = metadata['labHeadEmail']
+            lab_head_name = metadata['labHeadName']
+            pi_email = metadata['piEmail']
+            project_manager_name = metadata['projectManagerName']
+
+        event = ETLImportEvent(str(self.job.job_group.id),
+                               self.job.args['request_id'],
+                               list(samples_completed),
+                               list(samples_failed),
+                               recipe,
+                               data_analyst_email,
+                               data_analyst_name,
+                               investigator_email,
+                               investigator_name,
+                               lab_head_email,
+                               lab_head_name,
+                               pi_email,
+                               project_manager_name
+                               )
+        e = event.to_dict()
+        send_notification.delay(e)
+
+        etl_event = ETLJobsLinksEvent(str(self.job.job_group.id),
+                                      self.job.args['request_id'],
+                                      all_jobs)
+        etl_e = etl_event.to_dict()
+        send_notification.delay(etl_e)
+
+    def _generate_sample_data_file(self):
+        result = "SAMPLE_ID\tPATIENT_ID\tCOLLAB_ID\tSAMPLE_TYPE\tGENE_PANEL\tONCOTREE_CODE\tSAMPLE_CLASS\tSPECIMEN_PRESERVATION_TYPE\tSEX\tTISSUE_SITE\n"
+        ret_str = 'filemetadata__metadata__sampleId'
+        samples = File.objects.prefetch_related(Prefetch('filemetadata_set',
+                                                         queryset=FileMetadata.objects.select_related('file').order_by(
+                                                             '-created_date'))).filter(
+            filemetadata__metadata__requestId=self.job.args['request_id']).order_by(ret_str).distinct(ret_str).all()
+        for sample in samples:
+            metadata = sample.filemetadata_set.first().metadata
+            print(metadata)
+            result += '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(metadata['sampleId'],
+                                                                        metadata['patientId'],
+                                                                        metadata['investigatorSampleId'],
+                                                                        metadata['sampleClass'],
+                                                                        metadata['recipe'],
+                                                                        metadata['oncoTreeCode'],
+                                                                        metadata['specimenType'],
+                                                                        metadata['preservation'],
+                                                                        metadata['sex'],
+                                                                        metadata['tissueLocation'])
+        e = UploadAttachmentEvent(str(self.job.job_group.id),
+                                  '%s_sample_data_clinical.txt' % self.job.args['request_id'],
+                                  result).to_dict()
+        send_notification.delay(e)
+
+    def _job_failed(self):
+        # TODO: Hack remove this ASAP
+        if self.job.run == TYPES['REQUEST']:
+            import time
+            time.sleep(5)
+            ci_review = SetCIReviewEvent(str(self.job.job_group.id)).to_dict()
+            send_notification.delay(ci_review)
+            self._generate_ticket_decription()
+            self._generate_sample_data_file()
+
     def _job_successful(self):
         if self.job.run == TYPES['REQUEST']:
             # TODO: Hack remove this ASAP
             import time
             time.sleep(5)
-
-            samples_completed = set()
-            samples_failed = set()
-            all_jobs = []
-
-            jobs = Job.objects.filter(job_group=self.job.job_group.id).all()
-
-            for job in jobs:
-                if job.run == TYPES['SAMPLE']:
-                    if job.status == JobStatus.COMPLETED:
-                        samples_completed.add(job.args['sample_id'])
-                    elif job.status == JobStatus.FAILED:
-                        samples_failed.add(job.args['sample_id'])
-                all_jobs.append(str(job.id))
-
-            request_metadata = Job.objects.filter(args__request_id=self.job.args['request_id'], run=TYPES['SAMPLE']).first()
-
-            data_analyst_email = ""
-            data_analyst_name = ""
-            investigator_email = ""
-            investigator_name = ""
-            lab_head_email = ""
-            lab_head_name = ""
-            pi_email = ""
-            project_manager_name = ""
-            recipe = ""
-
-            if request_metadata:
-                metadata = request_metadata.args.get('request_metadata', {})
-                recipe = metadata['recipe']
-                data_analyst_email = metadata['dataAnalystEmail']
-                data_analyst_name = metadata['dataAnalystName']
-                investigator_email = metadata['investigatorEmail']
-                investigator_name = metadata['investigatorName']
-                lab_head_email = metadata['labHeadEmail']
-                lab_head_name = metadata['labHeadName']
-                pi_email = metadata['piEmail']
-                project_manager_name = metadata['projectManagerName']
-
-            event = ETLImportEvent(str(self.job.job_group.id),
-                                   self.job.args['request_id'],
-                                   list(samples_completed),
-                                   list(samples_failed),
-                                   recipe,
-                                   data_analyst_email,
-                                   data_analyst_name,
-                                   investigator_email,
-                                   investigator_name,
-                                   lab_head_email,
-                                   lab_head_name,
-                                   pi_email,
-                                   project_manager_name
-                                   )
-            e = event.to_dict()
-            send_notification.delay(e)
-            etl_event = ETLJobsLinksEvent(str(self.job.job_group.id),
-                                          self.job.args['request_id'],
-                                          all_jobs)
-            etl_e = etl_event.to_dict()
-            send_notification.delay(etl_e)
-
-            if len(Job.objects.filter(job_group=self.job.job_group.id, status=JobStatus.FAILED).all()) > 0:
-                ci_review = SetCIReviewEvent(str(self.job.job_group.id)).to_dict()
-                send_notification.delay(ci_review)
+            self._generate_ticket_decription()
+            self._generate_sample_data_file()
 
     def _check_children(self):
-        status = JobStatus.COMPLETED
-        message = []
+        finished = True
+        failed = []
         for child_id in self.job.children:
             try:
                 child_job = Job.objects.get(id=child_id)
             except Job.DoesNotExist:
-                status = JobStatus.FAILED
-                self.job.message = {"details": "Child job %s does't exist!" % child_id}
-                break
+                failed.append(child_id)
+                continue
             if child_job.status == JobStatus.FAILED:
-                status = JobStatus.FAILED
-                message.append(child_id)
-                break
+                failed.append(child_id)
             if child_job.status in (JobStatus.IN_PROGRESS, JobStatus.CREATED):
-                status = JobStatus.WAITING_FOR_CHILDREN
+                finished = False
                 break
-        self.job.status = status
-        if message:
-            self.job.message = {"details": "Child jobs %s failed" % ', '.join(message)}
-        if self.job.status == JobStatus.COMPLETED:
-            self._job_successful()
-            if self.job.callback:
-                job = Job(run=self.job.callback,
-                          args=self.job.callback_args,
-                          status=JobStatus.CREATED,
-                          max_retry=1,
-                          children=[],
-                          job_group=self.job.job_group)
-                job.save()
+        if finished:
+            if failed:
+                self.job.status = JobStatus.FAILED
+                self.job.message = {"details": "Child jobs %s failed" % ', '.join(failed)}
+                self._job_failed()
+            else:
+                self.job.status = JobStatus.COMPLETED
+                self._job_successful()
+                if self.job.callback:
+                    job = Job(run=self.job.callback,
+                              args=self.job.callback_args,
+                              status=JobStatus.CREATED,
+                              max_retry=1,
+                              children=[],
+                              job_group=self.job.job_group)
+                    job.save()
