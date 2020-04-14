09a3d56a494932e3e0bb8d4adfce2166f12551be
Adrian Fraiha <aef@fastmail.com>
2020-04-14 15:16:22 -0400

    Fixes file metadata page crashing due to too many files in dropdown (#220)
    * use name as str representation for file models
    
    * turn file in a searchable field so as not to crash the browser

--- file_system/admin.py
+++ file_system/admin.py
@@ -6,16 +6,21 @@ from .models import Storage, File, FileType, FileMetadata, FileGroup, FileGroupM
 
 class FileAdmin(admin.ModelAdmin):
     list_display = ('id', 'file_name', 'size')
+    search_fields = ['file_name']
 
 
 class FileRunMapAdmin(admin.ModelAdmin):
     list_display = ('file', 'run')
 
 
+class FileMetadataAdmin(admin.ModelAdmin):
+    list_display = ('file', 'version')
+    autocomplete_fields = ['file']
+
 admin.site.register(File, FileAdmin)
 admin.site.register(Storage)
 admin.site.register(FileGroup)
 admin.site.register(FileType)
-admin.site.register(FileMetadata)
+admin.site.register(FileMetadata, FileMetadataAdmin)
 admin.site.register(FileGroupMetadata)
 admin.site.register(FileRunMap, FileRunMapAdmin)
