2bfd2bf95c67712021663e8e793eed4eb4871ebf
Allan Bolipata <bolipatc@mskcc.org>
2020-04-16 15:29:54 -0400

    Added job_group_id to run_ids run request

--- runner/views/run_api_view.py
+++ runner/views/run_api_view.py
@@ -92,18 +92,30 @@ class OperatorViewSet(GenericAPIView):
         pipeline_name = request.data['pipeline_name']
         pipeline = get_object_or_404(Pipeline, name=pipeline_name)
 
-
-
         if request_ids:
             for request_id in request_ids:
                 logging.info("Submitting requestId %s to pipeline %s" % (request_id, pipeline_name))
                 create_jobs_from_request.delay(request_id, pipeline.operator_id)
             body = {"details": "Operator Job submitted %s" % str(request_ids)}
         else:
-            operator_model = Operator.objects.get(id=pipeline.operator_id)
-            operator = OperatorFactory.get_by_model(operator_model, run_ids=run_ids)
-            create_jobs_from_operator(operator)
-            body = {"details": "Operator Job submitted to pipeline %s with runs %s" % (pipeline_name, str(run_ids))}
+            if run_ids:
+                operator_model = Operator.objects.get(id=pipeline.operator_id)
+                operator = OperatorFactory.get_by_model(operator_model, run_ids=run_ids)
+                if job_group_id:
+                    create_jobs_from_operator(operator, job_group_id)
+                    body = {"details": "Operator Job submitted to pipeline %s, job group id %s,  with runs %s" % (pipeline_name, job_group_id,  str(run_ids))}
+                else:
+                    create_jobs_from_operator(operator)
+                    body = {"details": "Operator Job submitted to pipeline %s with runs %s" % (pipeline_name, str(run_ids))}
+            else:
+                operator_model = Operator.objects.get(id=pipeline.operator_id)
+                operator = OperatorFactory.get_by_model(operator_model, job_group_id=job_group_id)
+                if job_group_id:
+                    run_routine_operator_job(operator, job_group_id)
+                    body = {"details": "Operator Job submitted to operator %s (JobGroupId: %s)" % (operator, job_group_id)}
+                else:
+                    run_routine_operator_job(operator)
+                    body = {"details": "Operator Job submitted to operator %s" % operator}
         return Response(body, status=status.HTTP_200_OK)
 
 
