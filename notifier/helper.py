5243cee4be68990ba13b15fb5baf5155ef8e426f
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-05-18 15:45:22 -0400

    improve data clinical file

--- notifier/helper.py
+++ notifier/helper.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from file_system.repository.file_repository import FileRepository
 from runner.operator.roslin_operator.bin.make_sample import format_sample_name
 
@@ -26,4 +27,31 @@ def generate_sample_data_content(request_ids):
                 metadata['tissueLocation'],
                 metadata['sampleId']
             )
+    return result
+
+
+def generate_sample_data_content_new(files, pipeline_name, pipeline_github, pipeline_version):
+    result = "SAMPLE_ID\tPATIENT_ID\tCOLLAB_ID\tSAMPLE_TYPE\tGENE_PANEL\tONCOTREE_CODE\tSAMPLE_CLASS\tSPECIMEN_PRESERVATION_TYPE\tSEX\tTISSUE_SITE\tIGO_ID\tLAB_HEAD_EMAIL\tPIPELINE_NAME\tPIPELINE_GITHUB_LINK\tPIPELINE_VERSION\n"
+    ret_str = 'metadata__sampleId'
+    samples = FileRepository.filter(path_in=files, file_group=settings.IMPORT_FILE_GROUP).order_by(ret_str).distinct(ret_str).all()
+    print(samples)
+    for sample in samples:
+        metadata = sample.metadata
+        result += '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n'.format(
+            metadata.get('cmoSampleName', format_sample_name(metadata['sampleName'], metadata['specimenType'])),
+            metadata['patientId'],
+            metadata['investigatorSampleId'],
+            metadata['sampleClass'],
+            metadata['recipe'],
+            metadata['oncoTreeCode'],
+            metadata['specimenType'],
+            metadata['preservation'],
+            metadata['sex'],
+            metadata['tissueLocation'],
+            metadata['sampleId'],
+            metadata['labHeadEmail'],
+            pipeline_name,
+            pipeline_github,
+            pipeline_version
+        )
     return result
--- runner/operator/roslin_operator/roslin_operator.py
+++ runner/operator/roslin_operator/roslin_operator.py
@@ -2,11 +2,13 @@ import uuid
 from rest_framework import serializers
 from runner.operator.operator import Operator
 from runner.serializers import APIRunCreateSerializer
+from runner.models import Pipeline
 from .construct_roslin_pair import construct_roslin_jobs
 from .bin.pair_request import compile_pairs
 from .bin.make_sample import build_sample
 from notifier.events import UploadAttachmentEvent
 from notifier.tasks import send_notification
+from notifier.helper import generate_sample_data_content_new
 from runner.run.processors.file_processor import FileProcessor
 from file_system.repository.file_repository import FileRepository
 
@@ -17,6 +19,7 @@ class RoslinOperator(Operator):
         files = FileRepository.filter(queryset=self.files,
                                       metadata={'requestId': self.request_id,
                                                 'igocomplete': True})
+
         roslin_jobs = list()
 
         data = list()
@@ -28,6 +31,7 @@ class RoslinOperator(Operator):
             sample['metadata'] = f.metadata
             data.append(sample)
 
+        files = list()
         samples = list()
         # group by igoId
         igo_id_group = dict()
@@ -45,39 +49,54 @@ class RoslinOperator(Operator):
 
         sample_pairing = ""
         sample_mapping = ""
+        pipeline = self.get_pipeline_id()
+
+        try:
+            pipeline_obj = Pipeline.objects.get(id=pipeline)
+        except Pipeline.DoesNotExist:
+            pass
 
         for i, job in enumerate(roslin_inputs):
             tumor_sample_name = job['pair'][0]['ID']
             for p in job['pair'][0]['R1']:
                 sample_mapping += "\t".join(
                     [tumor_sample_name, FileProcessor.parse_path_from_uri(p['location'])]) + "\n"
+                files.append(FileProcessor.parse_path_from_uri(p['location']))
             for p in job['pair'][0]['R2']:
                 sample_mapping += "\t".join(
                     [tumor_sample_name, FileProcessor.parse_path_from_uri(p['location'])]) + "\n"
+                files.append(FileProcessor.parse_path_from_uri(p['location']))
             for p in job['pair'][0]['zR1']:
                 sample_mapping += "\t".join(
                     [tumor_sample_name, FileProcessor.parse_path_from_uri(p['location'])]) + "\n"
+                files.append(FileProcessor.parse_path_from_uri(p['location']))
             for p in job['pair'][0]['zR2']:
                 sample_mapping += "\t".join(
                     [tumor_sample_name, FileProcessor.parse_path_from_uri(p['location'])]) + "\n"
+                files.append(FileProcessor.parse_path_from_uri(p['location']))
 
             normal_sample_name = job['pair'][1]['ID']
             for p in job['pair'][1]['R1']:
                 sample_mapping += "\t".join(
                     [normal_sample_name, FileProcessor.parse_path_from_uri(p['location'])]) + "\n"
+                files.append(FileProcessor.parse_path_from_uri(p['location']))
             for p in job['pair'][1]['R2']:
                 sample_mapping += "\t".join(
                     [normal_sample_name, FileProcessor.parse_path_from_uri(p['location'])]) + "\n"
+                files.append(FileProcessor.parse_path_from_uri(p['location']))
             for p in job['pair'][1]['zR1']:
                 sample_mapping += "\t".join(
                     [normal_sample_name, FileProcessor.parse_path_from_uri(p['location'])]) + "\n"
+                files.append(FileProcessor.parse_path_from_uri(p['location']))
             for p in job['pair'][1]['zR2']:
                 sample_mapping += "\t".join(
                     [normal_sample_name, FileProcessor.parse_path_from_uri(p['location'])]) + "\n"
+                files.append(FileProcessor.parse_path_from_uri(p['location']))
 
             for p in job['pair'][1]['bam']:
                 sample_mapping += "\t".join(
                     [normal_sample_name, FileProcessor.parse_path_from_uri(p['location'])]) + "\n"
+                files.append(FileProcessor.parse_path_from_uri(p['location']))
 
             name = "ROSLIN %s, %i of %i" % (self.request_id, i + 1, number_of_inputs)
             assay = job['assay']
@@ -87,7 +106,7 @@ class RoslinOperator(Operator):
             sample_pairing += "\t".join([normal_sample_name, tumor_sample_name]) + "\n"
 
             roslin_jobs.append((APIRunCreateSerializer(
-                data={'app': self.get_pipeline_id(), 'inputs': roslin_inputs, 'name': name,
+                data={'app': pipeline, 'inputs': roslin_inputs, 'name': name,
                       'tags': {'requestId': self.request_id,
                           'sampleNameTumor': tumor_sample_name,
                           'sampleNameNormal': normal_sample_name,
@@ -99,5 +118,12 @@ class RoslinOperator(Operator):
 
         mapping_file_event = UploadAttachmentEvent(self.job_group_id, 'sample_mapping.txt', sample_mapping).to_dict()
         send_notification.delay(mapping_file_event)
+
+        data_clinical = generate_sample_data_content_new(files, pipeline_name=pipeline_obj.name,
+                                                         pipeline_github=pipeline_obj.github,
+                                                         pipeline_version=pipeline_obj.version)
+        sample_data_clinical_event = UploadAttachmentEvent(self.job_group_id, 'sample_data_clinical.txt',
+                                                           data_clinical).to_dict()
+        send_notification.delay(sample_data_clinical_event)
 
         return roslin_jobs
