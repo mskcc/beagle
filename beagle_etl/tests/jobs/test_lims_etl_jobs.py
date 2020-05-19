1efe18915cdddf388b1fcdd596307f080b82ddb3
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-05-19 10:44:04 -0400

    add tests for featch_sample_metadata

--- beagle_etl/tests/jobs/test_lims_etl_jobs.py
+++ beagle_etl/tests/jobs/test_lims_etl_jobs.py
@@ -1,18 +1,22 @@
 """
 Tests for LIMS ETL jobs
 """
+
 import os
-import beagle_etl.celery
+import json
+from mock import patch
 from unittest import skipIf
 from uuid import UUID
 from django.test import TestCase
 from django.conf import settings
 from beagle_etl.tasks import scheduler
 from beagle_etl.models import JobStatus, Job
-from beagle_etl.jobs.lims_etl_jobs import fetch_samples
-from beagle_etl.jobs.lims_etl_jobs import create_pooled_normal
-from beagle_etl.jobs.lims_etl_jobs import get_run_id_from_string
+from beagle_etl.exceptions import FailedToFetchFilesException
+from rest_framework.test import APITestCase
+from file_system.repository import FileRepository
 from file_system.models import File, FileMetadata
+from file_system.models import FileGroup, Storage, StorageType
+from beagle_etl.jobs.lims_etl_jobs import create_pooled_normal, fetch_sample_metadata, get_run_id_from_string, fetch_samples
 
 # use local execution for Celery tasks
 # if beagle_etl.celery.app.conf['task_always_eager'] == False:
@@ -81,6 +85,7 @@ class TestFetchSamples(TestCase):
         self.assertTrue(len(import_files_metadata) == 10)
         self.assertTrue(len(pooled_normal_files) == 12)
         self.assertTrue(len(pooled_normal_files_metadata) == 12)
+
 
 class TestCreatePooledNormal(TestCase):
     # load fixtures for the test case temp db
@@ -153,3 +158,227 @@ class TestGetRunID(TestCase):
         string = "BHFTCNBBXY"
         runID = get_run_id_from_string(string)
         self.assertTrue(runID == 'BHFTCNBBXY')
+
+
+class MockResponse:
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
+
+
+class TestImportSample(APITestCase):
+
+    def setUp(self):
+        self.storage = Storage.objects.create(name="LOCAL", type=StorageType.LOCAL)
+        self.job_group = FileGroup.objects.create(name="LIMS", storage=self.storage)
+        self.old_val = settings.IMPORT_FILE_GROUP
+        settings.IMPORT_FILE_GROUP = str(self.job_group.id)
+        self.data_1_fastq = [
+            {
+                "igoId": "igoId_001",
+                "cmoSampleName": "sampleName_001-d",
+                "sampleName": "sampleName_001-d",
+                "cmoSampleClass": "Normal",
+                "cmoPatientId": "patientId-001",
+                "investigatorSampleId": "InvestigatorSampleId-N01-WES",
+                "oncoTreeCode": None,
+                "tumorOrNormal": "Normal",
+                "tissueLocation": "na",
+                "specimenType": "Blood",
+                "sampleOrigin": "Whole Blood",
+                "preservation": "Blood",
+                "collectionYear": "2016",
+                "sex": "M",
+                "species": "Human",
+                "cfDNA2dBarcode": None,
+                "baitSet": "SureSelect-All-Exon-V4-hg19",
+                "qcReports": [],
+                "libraries": [
+                    {
+                        "barcodeId": "IDT36",
+                        "barcodeIndex": "CCAGTTCA",
+                        "libraryIgoId": "igoId_001_1",
+                        "libraryVolume": None,
+                        "libraryConcentrationNgul": 2.2051049976353,
+                        "dnaInputNg": None,
+                        "captureConcentrationNm": None,
+                        "captureInputNg": None,
+                        "captureName": None,
+                        "runs": [
+                            {
+                                "runMode": "HiSeq High Output",
+                                "runId": "runId_001",
+                                "flowCellId": "HHJ5HBBXX",
+                                "readLength": "",
+                                "runDate": "2017-04-21",
+                                "flowCellLanes": [
+                                    8
+                                ],
+                                "fastqs": [
+                                    "/path/to/sample/10/sampleName_001-d_IGO_igoId_002_S728_L008_R2_001.fastq.gz",
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+        self.data_2_fastq = [
+            {
+                "igoId": "igoId_002",
+                "cmoSampleName": "sampleName_002-d",
+                "sampleName": "sampleName_002-d",
+                "cmoSampleClass": "Normal",
+                "cmoPatientId": "patientId-002",
+                "investigatorSampleId": "InvestigatorSampleId-N01-WES",
+                "oncoTreeCode": None,
+                "tumorOrNormal": "Normal",
+                "tissueLocation": "na",
+                "specimenType": "Blood",
+                "sampleOrigin": "Whole Blood",
+                "preservation": "Blood",
+                "collectionYear": "2016",
+                "sex": "M",
+                "species": "Human",
+                "cfDNA2dBarcode": None,
+                "baitSet": "SureSelect-All-Exon-V4-hg19",
+                "qcReports": [],
+                "libraries": [
+                    {
+                        "barcodeId": "IDT36",
+                        "barcodeIndex": "CCAGTTCA",
+                        "libraryIgoId": "igoId_002_1",
+                        "libraryVolume": None,
+                        "libraryConcentrationNgul": 2.2051049976353,
+                        "dnaInputNg": None,
+                        "captureConcentrationNm": None,
+                        "captureInputNg": None,
+                        "captureName": None,
+                        "runs": [
+                            {
+                                "runMode": "HiSeq High Output",
+                                "runId": "runId_002",
+                                "flowCellId": "HHJ5HBBXX",
+                                "readLength": "",
+                                "runDate": "2017-04-21",
+                                "flowCellLanes": [
+                                    8
+                                ],
+                                "fastqs": [
+                                    "/path/to/sample/08/sampleName_002-d_IGO_igoId_002_S134_L008_R2_001.fastq.gz",
+                                    "/path/to/sample/08/sampleName_002-d_IGO_igoId_002_S134_L008_R1_001.fastq.gz"
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+        self.data_6_fastq = [
+            {
+                "igoId": "igoId_006",
+                "cmoSampleName": "sampleName_006",
+                "sampleName": "sampleName_006",
+                "cmoSampleClass": "Metastasis",
+                "cmoPatientId": "patientId_006",
+                "investigatorSampleId": "InvestigatorSampleId-T01-WES",
+                "oncoTreeCode": "PRAD",
+                "tumorOrNormal": "Tumor",
+                "tissueLocation": "Prostate",
+                "specimenType": "Resection",
+                "sampleOrigin": "Block",
+                "preservation": "FFPE",
+                "collectionYear": "2015",
+                "sex": "M",
+                "species": "Human",
+                "cfDNA2dBarcode": None,
+                "baitSet": "SureSelect-All-Exon-V4-hg19",
+                "qcReports": [],
+                "libraries": [
+                    {
+                        "barcodeId": "IDT59",
+                        "barcodeIndex": "GTGTTCTA",
+                        "libraryIgoId": "igoId_006_1",
+                        "libraryVolume": None,
+                        "libraryConcentrationNgul": 1.89855144270164,
+                        "dnaInputNg": None,
+                        "captureConcentrationNm": None,
+                        "captureInputNg": None,
+                        "captureName": None,
+                        "runs": [
+                            {
+                                "runMode": "HiSeq High Output",
+                                "runId": "runId_0061",
+                                "flowCellId": "HHGYTBBXX",
+                                "readLength": "",
+                                "runDate": "2017-04-25",
+                                "flowCellLanes": [
+                                    6,
+                                    7
+                                ],
+                                "fastqs": [
+                                    "/path/to/sample/01/sampleName_006_IGO_igoId_006_S64_L007_R2_001.fastq.gz",
+                                    "/path/to/sample/01/sampleName_006_IGO_igoId_006_S64_L007_R1_001.fastq.gz",
+                                    "/path/to/sample/01/sampleName_006_IGO_igoId_006_S64_L006_R1_001.fastq.gz",
+                                    "/path/to/sample/01/sampleName_006_IGO_igoId_006_S64_L006_R2_001.fastq.gz"
+                                ]
+                            },
+                            {
+                                "runMode": "HiSeq High Output",
+                                "runId": "runId_0062",
+                                "flowCellId": "HHN7YBBXX",
+                                "readLength": "",
+                                "runDate": "2017-05-05",
+                                "flowCellLanes": [
+                                    3
+                                ],
+                                "fastqs": [
+                                    "/path/to/sample/02/sampleName_006_IGO_igoId_006_S54_L003_R1_001.fastq.gz",
+                                    "/path/to/sample/02/sampleName_006_IGO_igoId_006_S54_L003_R2_001.fastq.gz"
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+
+    def tearDown(self):
+        settings.IMPORT_FILE_GROUP = self.old_val
+
+    @patch('requests.get')
+    def test_import_sample_two_fastq_files(self, mock_get_sample):
+        mock_get_sample.return_value = MockResponse(json_data=self.data_2_fastq, status_code=200)
+        fetch_sample_metadata('igoId_002', True, 'sampleName_002', {})
+        count_files = FileRepository.filter(path_in=[
+                                    "/path/to/sample/08/sampleName_002-d_IGO_igoId_002_S134_L008_R2_001.fastq.gz",
+                                    "/path/to/sample/08/sampleName_002-d_IGO_igoId_002_S134_L008_R1_001.fastq.gz"
+                                ]).count()
+        self.assertEqual(count_files, 2)
+
+    @patch('requests.get')
+    def test_import_sample_six_fastq_files(self, mock_get_sample):
+        mock_get_sample.return_value = MockResponse(json_data=self.data_6_fastq, status_code=200)
+        fetch_sample_metadata('igoId_006', True, 'sampleName_006', {})
+        count_files = FileRepository.filter(path_in=[
+            "/path/to/sample/01/sampleName_006_IGO_igoId_006_S64_L007_R2_001.fastq.gz",
+            "/path/to/sample/01/sampleName_006_IGO_igoId_006_S64_L007_R1_001.fastq.gz",
+            "/path/to/sample/01/sampleName_006_IGO_igoId_006_S64_L006_R1_001.fastq.gz",
+            "/path/to/sample/01/sampleName_006_IGO_igoId_006_S64_L006_R2_001.fastq.gz",
+            "/path/to/sample/02/sampleName_006_IGO_igoId_006_S54_L003_R1_001.fastq.gz",
+            "/path/to/sample/02/sampleName_006_IGO_igoId_006_S54_L003_R2_001.fastq.gz"
+        ]).count()
+        self.assertEqual(count_files, 6)
+
+    @patch('requests.get')
+    def test_invalid_number_of_fastq_files(self, mock_get_sample):
+        mock_get_sample.return_value = MockResponse(json_data=self.data_1_fastq, status_code=200)
+        with self.assertRaises(FailedToFetchFilesException) as e:
+            fetch_sample_metadata('igoId_001', True, 'sampleName_001', {})
+        count_files = FileRepository.filter(path_in=[
+            "/path/to/sample/10/sampleName_001-d_IGO_igoId_002_S728_L008_R2_001.fastq.gz"
+        ]).count()
+        self.assertEqual(count_files, 0)
