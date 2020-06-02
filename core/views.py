e9b0f089f4fa87265d77d1b185aaedadc4191ec1
Nikhil <nikhilkumar516@gmail.com>
2020-06-01 20:13:55 -0400

    Fixed JWT response payload

--- core/views.py
+++ core/views.py
@@ -2,14 +2,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
-def jwt_response_payload_handler(token, user=None, request=None):
-    return {
-        'token': token,
-        'user': {
-            'email': user.email,
-            'groups': user.mskuser.groups.split(',')
-        }
-    }
 
 
 class BeagleTokenObtainPairSerializer(TokenObtainPairSerializer):
