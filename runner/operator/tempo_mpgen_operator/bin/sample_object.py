17e69c54d6716773062213eb575e151300860646
Allan Bolipata <bolipatc@mskcc.org>
2020-06-03 18:56:48 -0400

    Adding sample and patient ojb for tempo

--- runner/operator/tempo_mpgen_operator/bin/sample_object.py
+++ runner/operator/tempo_mpgen_operator/bin/sample_object.py
@@ -44,14 +44,14 @@ class Fastqs:
         self.r1 = list()
         self.r2 = list()
         self.paired = True
-        self.set_R(file_list)
+        self._set_R(file_list)
 
 
-    def set_R(self, file_list):
+    def _set_R(self, file_list):
         """
         From the file list, retrieve R1 and R2 fastq files
 
-        Uses get_fastq_from_list() to find R2 pair.
+        Uses _get_fastq_from_list() to find R2 pair.
         """
         r1s = list()
         r2s = list()
@@ -66,7 +66,7 @@ class Fastqs:
             self.r1.append(f)
             fastq1 = f.path
             expected_r2 = 'R2'.join(fastq1.rsplit('R1', 1))
-            fastq2 = self.get_fastq_from_list(expected_r2, r2s)
+            fastq2 = self._get_fastq_from_list(expected_r2, r2s)
             if fastq2:
                 self.r2.append(fastq2)
             else:
@@ -74,7 +74,17 @@ class Fastqs:
                 self.paired = False
 
 
-    def get_fastq_from_list(self, fastq_path, fastq_files):
+    def __str__(self):
+        s = "R1:\n"
+        for i in self.r1:
+            s += i.path +"\n"
+        s += "\nR2:\n"
+        for i in self.r2:
+            s += i.path + "\n"
+        return s
+
+
+    def _get_fastq_from_list(self, fastq_path, fastq_files):
         """
         Given fastq_path, find it in the list of fastq_files and return
         that File object
