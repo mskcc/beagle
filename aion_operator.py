834ea490e52ea1a612e741644de9db36613cdcd2
Allan Bolipata <bolipatc@mskcc.org>
2020-06-18 18:21:08 -0400

    aion operator draft

---
+++ aion_operator.py
@@ -0,0 +1,75 @@
+"""
+Helix Filters Operator
+
+Constructs input JSON for the Helix Filters pipeline and then
+submits them as runs
+"""
+import os
+import datetime
+import logging
+from notifier.models import JobGroup
+from runner.models import Run
+from runner.operator.operator import Operator
+from runner.serializers import APIRunCreateSerializer
+from runner.models import Pipeline
+LOGGER = logging.getLogger(__name__)
+
+
+def get_jobs(run_ids):
+    """
+    From self, retrieve relevant run IDs, build the input JSON for
+    the pipeline, and then submit them as jobs through the
+    APIRunCreateSerializer
+    """
+    number_of_runs = len(run_ids)
+    name = "AION OUTPUTS %s runs [%s,..] " % (
+        number_of_runs, run_ids[0])
+    input_json = build_input_json(run_ids)
+    return input_json
+
+
+def build_input_json(run_ids):
+#    run_ids = self.run_ids
+    project_prefixes = set()
+    directories = set()
+    lab_heads = set()
+    argos_run_ids = set()
+
+    for run_id in run_ids:
+        run = Run.objects.get(id=run_id)
+        print(run_id)
+        argos_run_ids = run.tags['run_ids']
+        project_prefix = run.tags['project_prefix']
+        if project_prefix:
+            project_prefixes.add(project_prefix)
+        run_portal_dir = os.path.join(run.output_directory, "portal")
+        if os.path.isdir(run_portal_dir):
+            directories.add(run_portal_dir)
+    merge_date = datetime.datetime.now().strftime("%Y_%m_%d")
+    lab_head_email = get_lab_head(argos_run_ids)
+    input_json = dict()
+    lab_head_id = lab_head_email.split("@")[0]
+    study_id = "set_%s" % lab_head_id 
+    input_json["project_description"] = "[%s] Includes samples received at IGO for Project ID(s): %s" % (merge_date, ", ".join(sorted(project_prefixes)))
+    input_json["study_id"] = study_id
+    input_json["project_title"] = "CMO Merged Study for Principal Investigator (%s)" % study_id
+    input_json["directories"] = list()
+    for portal_directory in directories:
+        input_json["directories"].append({"class": "Directory", "path": portal_directory})
+    return input_json
+
+
+def get_lab_head(argos_run_ids):
+    lab_head_emails = set()
+    for argos_run_id in argos_run_ids:
+        argos_run = Run.objects.get(id=argos_run_id)
+        lab_head_email = argos_run.tags['labHeadEmail']
+        if lab_head_email:
+            lab_head_emails.add(lab_head_email)
+    if len(lab_head_emails) > 1:
+#         LOGGER.warn("Multiple lab head emails found; merge output directory unclear.")
+        print("Multiple lab head emails found; merge output directory unclear.")
+    # TODO: get clarity on where to output if there are multiple pi found
+    if lab_head_emails:
+         return sorted(lab_head_emails)[0]
+    return None
