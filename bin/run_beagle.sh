d31d243a4fa380d76a26acc7c3927b72a683df90
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-06-22 16:36:31 -0400

    fix typo

--- bin/run_beagle.sh
+++ bin/run_beagle.sh
@@ -26,8 +26,8 @@ singularity instance stop $1_beagle_celery
 echo Starting $1 environment version $3
 
 if [ $1 != "production" ]; then
-    singularity instance start --containall --bind /srv/services/voyager/ --bind /home/ivkovic/rabix-cli-1.0.5 --bind /tmp --bind /ifs --bind /juno/work/ci /srv/services/voyager/beagle_service_%3.sif $1_beagle_services
-    singularity instance start --containall --bind /srv/services/voyager/ --bind /home/ivkovic/rabix-cli-1.0.5 --bind /tmp --bind /ifs --bind /juno/work/ci /srv/services/voyager/beagle_celery_%3.sif $1_beagle_celery
+    singularity instance start --containall --bind /srv/services/voyager/ --bind /home/ivkovic/rabix-cli-1.0.5 --bind /tmp --bind /ifs --bind /juno/work/ci /srv/services/voyager/beagle_service_$3.sif $1_beagle_services
+    singularity instance start --containall --bind /srv/services/voyager/ --bind /home/ivkovic/rabix-cli-1.0.5 --bind /tmp --bind /ifs --bind /juno/work/ci /srv/services/voyager/beagle_celery_$3.sif $1_beagle_celery
 else
     singularity instance start --containall --bind /srv/services/staging_voyager/ --bind /home/ivkovic/rabix-cli-1.0.5 --bind /tmp --bind /ifs --bind /juno/work/ci /srv/services/staging_voyager/beagle_service_$3.sif $1_beagle_services
     singularity instance start --containall --bind /srv/services/staging_voyager/ --bind /home/ivkovic/rabix-cli-1.0.5 --bind /tmp --bind /ifs --bind /juno/work/ci /srv/services/staging_voyager/beagle_celery_$3.sif $1_beagle_celery
