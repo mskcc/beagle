66e542d906f6531d565d3e0fc06e58645878c6c3
Allan Bolipata <bolipatc@mskcc.org>
2020-07-01 20:25:08 -0400

    Removing normals from generated data clinical file in helix filters operator

--- runner/operator/helix_filters/construct_helix_filters_input.py
+++ runner/operator/helix_filters/construct_helix_filters_input.py
@@ -212,16 +212,6 @@ def get_files_from_run(r):
         files.append(FileProcessor.get_file_path(p['location']))
     for p in inp_port.db_value[0]['zR2']:
         files.append(FileProcessor.get_file_path(p['location']))
-    for p in inp_port.db_value[1]['R1']:
-        files.append(FileProcessor.get_file_path(p['location']))
-    for p in inp_port.db_value[1]['R2']:
-        files.append(FileProcessor.get_file_path(p['location']))
-    for p in inp_port.db_value[1]['zR1']:
-        files.append(FileProcessor.get_file_path(p['location']))
-    for p in inp_port.db_value[1]['zR2']:
-        files.append(FileProcessor.get_file_path(p['location']))
-    for p in inp_port.db_value[1]['bam']:
-        files.append(FileProcessor.get_file_path(p['location']))
     return files
 
 
