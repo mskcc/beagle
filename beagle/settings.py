b91f2bec65cc2b8ac62e25a384b21fba370b3e47
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-07-27 13:41:46 -0400

    Fix formating and setting pipeline field

--- beagle/settings.py
+++ beagle/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4gm1)1&0x71+^vwo)rf=%%b)f3l$%u893bs$scif+h#nj@eyx('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('BEAGLE_ALLOWED_HOSTS', 'localhost').split(',')
 
@@ -329,6 +329,7 @@ JIRA_URL = os.environ.get("JIRA_URL", "")
 JIRA_USERNAME = os.environ.get("JIRA_USERNAME", "")
 JIRA_PASSWORD = os.environ.get("JIRA_PASSWORD", "")
 JIRA_PROJECT = os.environ.get("JIRA_PROJECT", "")
+JIRA_PIPELINE_FIELD_ID = os.environ.get('JIRA_PIPELINE_FIELD_ID', "customfield_10901")
 
 BEAGLE_URL = os.environ.get('BEAGLE_URL', 'http://silo:5001')
 
