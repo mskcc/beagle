12c711a876226c31220b0128fea897904dd18ec6
Nikhil <nikhilkumar516@gmail.com>
2020-08-17 12:20:55 -0400

    Moved operator into versioned directories

--- runner/operator/__init__.py
+++ runner/operator/__init__.py
@@ -1,8 +1,9 @@
-from .argos_operator import argos_operator
-from .copy_outputs_operator import copy_outputs_operator
-from .argos_qc_operator import argos_qc_operator
-from .tempo_operator import tempo_operator
-from .access_operator import access_operator
+from .argos_operator.v1_0_0 import argos_operator as argos_operator_v1_0_0
+from .copy_outputs_operator.v1_0_0 import copy_outputs_operator as copy_outputs_operator_v1_0_0
+from .argos_qc_operator.v1_0_0 import argos_qc_operator as argos_qc_operator_v1_0_0
+from .tempo_operator.v1_0_0 import tempo_operator as tempo_operator_v1_0_0
+from .access_operator.v1_0_0 import access_operator as access_operator_v1_0_0
+from .helix_filters.v1_0_0 import helix_filters_operator as helix_filters_operator_v1_0_0
+from .aion.v1_0_0 import aion_operator as aion_operator_v1_0_0
 from .operator_factory import OperatorFactory
-from .helix_filters import helix_filters_operator
-from .aion import aion_operator
+
