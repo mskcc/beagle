e3fe12f0f320b8b90f96aa1f0ab32c8145780770
Nikhil <nikhilkumar516@gmail.com>
2020-08-17 18:35:30 -0400

    Fixed operator folder __init__

--- runner/operator/__init__.py
+++ runner/operator/__init__.py
@@ -1,9 +1,10 @@
-from .argos_operator.v1_0_0 import argos_operator as argos_operator_v1_0_0
-from .copy_outputs_operator.v1_0_0 import copy_outputs_operator as copy_outputs_operator_v1_0_0
-from .argos_qc_operator.v1_0_0 import argos_qc_operator as argos_qc_operator_v1_0_0
-from .tempo_operator.v1_0_0 import tempo_operator as tempo_operator_v1_0_0
-from .access_operator.v1_0_0 import access_operator as access_operator_v1_0_0
-from .helix_filters.v1_0_0 import helix_filters_operator as helix_filters_operator_v1_0_0
-from .aion.v1_0_0 import aion_operator as aion_operator_v1_0_0
+from .argos_operator.v1_0_0 import ArgosOperator as argos_operator_v1_0_0
+from .copy_outputs_operator.v1_0_0 import CopyOutputsOperator as copy_outputs_operator_v1_0_0
+from .argos_qc_operator.v1_0_0 import ArgosQcOperator as argos_qc_operator_v1_0_0
+from .tempo_operator.v1_0_0 import TempoOperator as tempo_operator_v1_0_0
+from .access_operator.v1_0_0 import AccessOperator as access_operator_v1_0_0
+from .helix_filters.v1_0_0 import HelixFiltersOperator as helix_filters_operator_v1_0_0
+from .aion.v1_0_0 import AionOperator as aion_operator_v1_0_0
+from .access.v1_0_0.fastq_to_bam import AccessFastqToBamOperator as AccessFastqToBamOperator_v1_0_0
 from .operator_factory import OperatorFactory
 
