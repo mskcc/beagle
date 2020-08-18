8ac1b70e6056233a177a2fb2415cb92f4a35c883
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-08-18 13:22:46 -0400

    rename imports to camelcase

--- runner/operator/__init__.py
+++ runner/operator/__init__.py
@@ -1,10 +1,10 @@
-from .argos_operator.v1_0_0 import ArgosOperator as argos_operator_v1_0_0
-from .copy_outputs_operator.v1_0_0 import CopyOutputsOperator as copy_outputs_operator_v1_0_0
-from .argos_qc_operator.v1_0_0 import ArgosQcOperator as argos_qc_operator_v1_0_0
-from .tempo_operator.v1_0_0 import TempoOperator as tempo_operator_v1_0_0
-from .access_operator.v1_0_0 import AccessOperator as access_operator_v1_0_0
-from .helix_filters.v20_07_1 import HelixFiltersOperator as helix_filters_operator_v20_07_1
-from .aion.v1_0_0 import AionOperator as aion_operator_v1_0_0
+from .argos_operator.v1_0_0 import ArgosOperator as ArgosOperator_v1_0_0
+from .copy_outputs_operator.v1_0_0 import CopyOutputsOperator as CopyOutputsOperator_v1_0_0
+from .argos_qc_operator.v1_0_0 import ArgosQcOperator as ArgosQcOperator_v1_0_0
+from .tempo_operator.v1_0_0 import TempoOperator as TempoOperator_v1_0_0
+from .access_operator.v1_0_0 import AccessOperator as AccessOperator_v1_0_0
+from .helix_filters.v20_07_1 import HelixFiltersOperator as HelixFiltersOperator_v20_07_1
+from .aion.v1_0_0 import AionOperator as AionOperator_v1_0_0
 from .access.v1_0_0.fastq_to_bam import AccessFastqToBamOperator as AccessFastqToBamOperator_v1_0_0
 from .operator_factory import OperatorFactory
 
