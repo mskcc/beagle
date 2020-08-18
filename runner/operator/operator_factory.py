1ae5602a32b6f5961e98e698b455cb9e1963c788
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-08-18 13:22:46 -0400

    rename imports to camelcase

--- runner/operator/operator_factory.py
+++ runner/operator/operator_factory.py
@@ -1,10 +1,10 @@
-from .argos_operator.v1_0_0 import ArgosOperator as argos_operator_v1_0_0
-from .copy_outputs_operator.v1_0_0 import CopyOutputsOperator as copy_outputs_operator_v1_0_0
-from .argos_qc_operator.v1_0_0 import ArgosQcOperator as argos_qc_operator_v1_0_0
-from .tempo_operator.v1_0_0 import TempoOperator as tempo_operator_v1_0_0
-from .access_operator.v1_0_0 import AccessOperator as access_operator_v1_0_0
-from .helix_filters.v20_07_1 import HelixFiltersOperator as helix_filters_operator_v20_07_1
-from .aion.v1_0_0 import AionOperator as aion_operator_v1_0_0
+from .argos_operator.v1_0_0 import ArgosOperator as ArgosOperator_v1_0_0
+from .copy_outputs_operator.v1_0_0 import CopyOutputsOperator as CopyOutputsOperator_v1_0_0
+from .argos_qc_operator.v1_0_0 import ArgosQcOperator as ArgosQcOperator_v1_0_0
+from .tempo_operator.v1_0_0 import TempoOperator as TempoOperator_v1_0_0
+from .access_operator.v1_0_0 import AccessOperator as AccessOperator_v1_0_0
+from .helix_filters.v20_07_1 import HelixFiltersOperator as HelixFiltersOperator_v20_07_1
+from .aion.v1_0_0 import AionOperator as AionOperator_v1_0_0
 from .access.v1_0_0.fastq_to_bam import AccessFastqToBamOperator as AccessFastqToBamOperator_v1_0_0
 
 
@@ -12,28 +12,28 @@ class OperatorFactory(object):
 
     operators = {
         "TempoOperator": [
-            {"version": "v1.0.0", "latest": True, "operator": tempo_operator_v1_0_0}
+            {"version": "v1.0.0", "latest": True, "operator": TempoOperator_v1_0_0}
         ],
         "ArgosOperator": [
-            {"version": "v1.0.0", "latest": True, "operator": argos_operator_v1_0_0}
+            {"version": "v1.0.0", "latest": True, "operator": ArgosOperator_v1_0_0}
         ],
         "AccessOperator": [
-            {"version": "v1.0.0", "latest": True, "operator": access_operator_v1_0_0}
+            {"version": "v1.0.0", "latest": True, "operator": AccessOperator_v1_0_0}
         ],
         "ArgosQcOperator": [
-            {"version": "v1.0.0", "latest": True, "operator": argos_qc_operator_v1_0_0}
+            {"version": "v1.0.0", "latest": True, "operator": ArgosQcOperator_v1_0_0}
         ],
         "CopyOutputsOperator": [
-            {"version": "v1.0.0", "latest": True, "operator": copy_outputs_operator_v1_0_0}
+            {"version": "v1.0.0", "latest": True, "operator": CopyOutputsOperator_v1_0_0}
         ],
         "AccessFastqToBamOperator": [
             {"version": "v1.0.0", "latest": True, "operator": AccessFastqToBamOperator_v1_0_0}
         ],
         "HelixFiltersOperator": [
-            {"version": "v20.07.1", "latest": True, "operator": helix_filters_operator_v20_07_1}
+            {"version": "v20.07.1", "latest": True, "operator": HelixFiltersOperator_v20_07_1}
         ],
         "AionOperator": [
-            {"version": "v1.0.0", "latest": True, "operator": aion_operator_v1_0_0}
+            {"version": "v1.0.0", "latest": True, "operator": AionOperator_v1_0_0}
         ]
     }
 
