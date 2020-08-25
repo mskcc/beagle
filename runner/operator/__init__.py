38098e26a227096f0abb0fec3fd37eacab329782
Nikhil <nikhilkumar516@gmail.com>
2020-08-25 15:26:21 -0400

    Added helix_filters operator version v20.08.1

--- runner/operator/__init__.py
+++ runner/operator/__init__.py
@@ -4,6 +4,7 @@ from .argos_qc_operator.v1_0_0 import ArgosQcOperator as ArgosQcOperator_v1_0_0
 from .tempo_operator.v1_0_0 import TempoOperator as TempoOperator_v1_0_0
 from .access_operator.v1_0_0 import AccessOperator as AccessOperator_v1_0_0
 from .helix_filters.v20_07_1 import HelixFiltersOperator as HelixFiltersOperator_v20_07_1
+from .helix_filters.v20_08_1 import HelixFiltersOperator as HelixFiltersOperator_v20_08_1
 from .aion.v1_0_0 import AionOperator as AionOperator_v1_0_0
 from .access.v1_0_0.fastq_to_bam import AccessFastqToBamOperator as AccessFastqToBamOperator_v1_0_0
 from .operator_factory import OperatorFactory
