bacde8c8b435eb75832ddaff90b61b083eec03b1
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2020-10-19 18:08:26 -0400

    Implement UserRegistrationRequest endpoint

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from core.models import UserRegistrationRequest
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
@@ -15,5 +16,23 @@ class UserAdmin(BaseUserAdmin):
     inlines = (MskccUserInline,)
 
 
+def approve(modeladmin, request, queryset):
+    for req in queryset:
+        User.objects.create(username=req.username, email='%s@mskcc.org' % req.username, first_name=req.first_name,
+                            last_name=req.last_name)
+        req.approved = True
+        req.save()
+
+
+approve.short_description = "Approve request"
+
+
+class UserRegistrationRequestAdmin(admin.ModelAdmin):
+    list_display = ('username', 'approved',)
+    search_fields = ('username',)
+    actions = (approve,)
+
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(UserRegistrationRequest, UserRegistrationRequestAdmin)
