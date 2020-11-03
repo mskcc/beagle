94df5846cc49e445fc25930660478e14cde6999b
Allan Bolipata <bolipatc@mskcc.org>
2020-11-03 15:43:21 -0500

    #erqvee[In Progress] Add QC e-mails to JIRA description

--- notifier/events/etl_import_event.py
+++ notifier/events/etl_import_event.py
@@ -6,7 +6,7 @@ class ETLImportEvent(Event):
 
     def __init__(self, job_notifier, job_group, request_id, sample_list_completed, sample_list_fail, recipe,
                  data_analyst_email, data_analyst_name, investigator_email, investigator_name, lab_head_email,
-                 lab_head_name, pi_email, project_manager_name, number_of_tumors, number_of_normals,
+                 lab_head_name, pi_email, project_manager_name, qc_access_emails, number_of_tumors, number_of_normals,
                  number_of_pool_normals):
         self.job_notifier = job_notifier
         self.job_group = job_group
@@ -22,6 +22,7 @@ class ETLImportEvent(Event):
         self.lab_head_name = lab_head_name
         self.pi_email = pi_email
         self.project_manager_name = project_manager_name
+        self.qc_access_emails = qc_access_emails
         self.number_of_tumors = number_of_tumors
         self.number_of_normals = number_of_normals
         self.number_of_pool_normals = number_of_pool_normals
@@ -45,8 +46,9 @@ class ETLImportEvent(Event):
         Investigator e-mail: {investigator_email}
         Lab Head Name: {lab_head_name}
         Lab Head e-mail: {lab_head_email}
-        PI e-email: {pi_email}
+        PI E-mail: {pi_email}
         Project Manager Name: {project_manager_name}
+        QC E-mails: {qc_access_emails}
 
         {cnt_samples_completed} samples successfully imported
         {cnt_samples_fail} samples failed
@@ -70,6 +72,7 @@ class ETLImportEvent(Event):
                                                   lab_head_email=self.lab_head_email,
                                                   pi_email=self.pi_email,
                                                   project_manager_name=self.project_manager_name,
+                                                  qc_access_emails=self.qc_access_emails,
                                                   number_of_tumors=self.number_of_tumors,
                                                   number_of_normals=self.number_of_normals,
                                                   number_of_pool_normals=self.number_of_pool_normals,
