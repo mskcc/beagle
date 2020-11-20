97adca17becbc3f8a948a5ad466bf99f6c2f2d62
ionox0 <ionox0@gmail.com>
2020-11-20 17:42:42 -0500

    instead of looking through all past samples, SV pipeline should only run based on Request IDs

--- runner/operator/access/v1_0_0/structural_variants/__init__.py
+++ runner/operator/access/v1_0_0/structural_variants/__init__.py
@@ -24,62 +24,28 @@ ACCESS_DEFAULT_SV_NORMAL_FILENAME = 'DONOR22-TP_cl_aln_srt_MD_IR_FX_BR.bam'
 
 class AccessLegacySVOperator(Operator):
 
-    def get_tumors_to_run(self):
-        """
-        Find all runs through collapsing pipeline that have not been run through SNV pipeline
-
-        :return:
-        """
-        access_duplex_output_ports = Port.objects.filter(
-            name='duplex_bams',
-            run__app__name='access legacy',
-            run__status=RunStatus.COMPLETED
-        )
-        # Each port is a list, so need a double list comprehension here
-        all_access_output_records = [f for p in access_duplex_output_ports for f in p.value]
-        # these are port objects, they dont have metadata field
-        all_access_completed_samples = [r['sampleId'] for r in all_access_output_records]
-
-        access_snv_runs = Run.objects.filter(status=RunStatus.COMPLETED, app__name='access_legacy_sv')
-        already_ran_tumors = [r['tags']['cmoSampleIds'] for r in access_snv_runs]
-        already_ran_tumors = [item for sublist in already_ran_tumors for item in sublist]
-
-        tumors_to_run = set(all_access_completed_samples) - set(already_ran_tumors)
-        return list(tumors_to_run)
-
     def get_sample_inputs(self):
         """
         Create all sample inputs for all runs triggered in this instance of the operator
 
         :return: list of json_objects
         """
-        tumors_to_run = self.get_tumors_to_run()
-
-        tumor_standard_bams = []
-
-        for i, tumor_sample_id in enumerate(tumors_to_run):
-
-            tumor_standard_bam = FileRepository.filter(
-                file_type='bam',
-                path_regex='_cl_aln_srt_MD_IR_FX_BR.bam',
-                metadata={
-                    'tumorOrNormal': 'Tumor',
-                    'sampleName': tumor_sample_id
-                }
-            )
-            if not len(tumor_standard_bam) == 1:
-                msg = 'Found incorrect number of matching bam files ({}) for sample {}'
-                msg = msg.format(len(tumor_standard_bam), tumor_sample_id)
-                raise Exception(msg)
-            tumor_standard_bam = tumor_standard_bam[0]
-
-            tumor_standard_bams.append(tumor_standard_bam)
+        tumor_standard_bams = FileRepository.filter(
+            file_type='bam',
+            path_regex='_cl_aln_srt_MD_IR_FX_BR.bam',
+            metadata={
+                'requestId': self.request_id,
+                'tumorOrNormal': 'Tumor',
+                'igocomplete': True
+            }
+        )
+        sample_ids = [b.metadata['sampleName'] for b in tumor_standard_bams]
 
         sample_inputs = []
         for i, b in enumerate(tumor_standard_bams):
 
             sample_input = self.construct_sample_inputs(
-                tumors_to_run[i],
+                sample_ids[i],
                 b
             )
             sample_inputs.append(sample_input)
