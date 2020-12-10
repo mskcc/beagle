d0b3a058e77e28c239b672327a173de7ae5e876f
Adrian Fraiha <aef@fastmail.com>
2020-12-10 15:14:30 -0500

    Speed up run page + add export ability to files

--- beagle/settings.py
+++ beagle/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'import_export',
     'rest_framework',
     'corsheaders',
     'drf_multiple_model',
