b8d94801a18a68627688ca94742b266024815ae2
ionox0 <ionox0@gmail.com>
2020-12-22 18:17:23 -0500

    typo in merge-fastq operator

--- runner/operator/access/v1_0_0/legacy/__init__.py
+++ runner/operator/access/v1_0_0/legacy/__init__.py
@@ -145,7 +145,7 @@ def construct_sample_inputs(samples, request_id, group_id):
 
             # Todo: need to add metadata for "Read 1" and "Read 2" to fastq files
             r1_fastq = sample_pair[0] if '_R1_.fastq.gz' in sample_pair[0]["path"] else sample_pair[1]
-            r2_fastq = sample_pair[0] if '_R2_.fastz.gz' in sample_pair[0]["path"] else sample_pair[1]
+            r2_fastq = sample_pair[0] if '_R2_.fastq.gz' in sample_pair[0]["path"] else sample_pair[1]
 
             fastq1_files.append({
                 "class": "File",
