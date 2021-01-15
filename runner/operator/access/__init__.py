2d1ee50f5c3d0cf282b06a89b1a8e6d900cc2547
Ian <ionox0@gmail.com>
2021-01-15 10:38:07 -0500

    fix for matched normal searching not working (#668)
    * fix for matched normal searching not working
    
    * Handle case where matched normal is not found (skip genotyping)
    
    * use empty string in matched_normal_ids field
    
    * Implement DMP ACCESS normal search for SNV pipeline
    
    * Update to match actual DMP ACCESS bam file names
    
    * update comment
    
    * cleanup
    
    * todo

--- runner/operator/access/__init__.py
+++ runner/operator/access/__init__.py
@@ -1,4 +1,17 @@
+import logging
+
 from runner.models import Run, RunStatus
+from django.db.models import Prefetch, Q
+from file_system.repository.file_repository import FileRepository
+
+
+logger = logging.getLogger(__name__)
+
+ACCESS_CURATED_BAMS_FILE_GROUP_SLUG = 'access_curated_normals'
+ACCESS_DEFAULT_NORMAL_ID = 'DONOR22-TP'
+ACCESS_DEFAULT_NORMAL_FILENAME = r'DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam$'
+NORMAL_SAMPLE_SEARCH = '-N0'
+DMP_UNFILTERED_BAM_REGEX = r'-unfilter.bam$'
 
 
 def get_request_id_runs(request_id):
@@ -19,3 +32,62 @@ def get_request_id_runs(request_id):
         status=RunStatus.COMPLETED
     )
     return request_id_runs
+
+
+def get_unfiltered_matched_normal(patient_id):
+    """
+    Find a matched normal sample for the given patient ID with the following precedence:
+
+    1. Latest Matched Normal from IGO ACCESS samples
+    2. Latest Matched Normal from DMP ACCESS samples
+    3. Return (None, ''), which will be used as a placeholder for skipping genotyping in the SNV pipeline
+
+    Todo: generalize to work for duplex / simplex / standard, and use in MSI operator
+
+    :return: (file_system.models.File - bam, str - sample_id)
+    """
+    sample_regex = r'{}.*{}.*__aln_srt_IR_FX.bam$'.format(patient_id, NORMAL_SAMPLE_SEARCH)
+    unfiltered_matched_normal_bam = FileRepository.filter(path_regex=sample_regex)
+    if len(unfiltered_matched_normal_bam) == 0:
+        msg = 'WARNING: Could not find IGO matching unfiltered normal bam file for patient {}. ' \
+              'Searching for DMP sample.'
+        msg = msg.format(patient_id)
+        logger.warning(msg)
+
+        # Todo: need to specify ACCESS as assay?
+        # assay = Q(metadata__cmo_assay=value)
+        patient = Q(metadata__patient__cmo=patient_id.replace('C-', ''))
+        normal = Q(metadata__type='N')
+        query = patient & normal
+
+        # Unfiltered bams only
+        file_objs = FileRepository.filter(path_regex=DMP_UNFILTERED_BAM_REGEX)
+        unfiltered_matched_normal_bam = FileRepository.filter(queryset=file_objs, q=query)
+
+        if len(unfiltered_matched_normal_bam) == 0:
+            msg = 'WARNING: Could not find DMP or IGO matching unfiltered normal bam file for patient {}. ' \
+                  'We will skip running this sample.'
+            msg = msg.format(patient_id)
+            logger.warning(msg)
+            unfiltered_matched_normal_bam = None
+            unfiltered_matched_normal_sample_id = ''
+        else:
+            unfiltered_matched_normal_bam = unfiltered_matched_normal_bam.order_by('-metadata__imported').first()
+
+    elif len(unfiltered_matched_normal_bam) > 1:
+        msg = 'WARNING: Found more than one matching unfiltered normal bam file for patient {}. ' \
+              'We will choose the most recently-created one for this run.'
+        msg = msg.format(patient_id)
+        logger.warning(msg)
+        unfiltered_matched_normal_bam = unfiltered_matched_normal_bam.order_by('-created_date').first()
+
+    else:
+        unfiltered_matched_normal_bam = unfiltered_matched_normal_bam[0]
+
+    # Parse the Normal Sample ID from the file name
+    # Todo: Stop using file path for this, once output_metadata is being supplied in access legacy operator
+    if unfiltered_matched_normal_bam:
+        unfiltered_matched_normal_file_base = unfiltered_matched_normal_bam.file.path.split('/')[-1]
+        unfiltered_matched_normal_sample_id = '-'.join(unfiltered_matched_normal_file_base.split('-')[0:3])
+
+    return unfiltered_matched_normal_bam, unfiltered_matched_normal_sample_id
