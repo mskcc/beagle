dcf45e8cd520d7a2ed803cb3f000e41270150d7d
ionox0 <ionox0@gmail.com>
2021-01-20 18:34:16 -0500

    Use startswith and endswith to avoid regex and speed up File queries

--- runner/operator/access/__init__.py
+++ runner/operator/access/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 from runner.models import Run, RunStatus
 from django.db.models import Prefetch, Q
+from file_system.models import File
 from file_system.repository.file_repository import FileRepository
 
 
@@ -9,9 +10,10 @@ logger = logging.getLogger(__name__)
 
 ACCESS_CURATED_BAMS_FILE_GROUP_SLUG = 'access_curated_normals'
 ACCESS_DEFAULT_NORMAL_ID = 'DONOR22-TP'
-ACCESS_DEFAULT_NORMAL_FILENAME = r'DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam$'
+ACCESS_DEFAULT_NORMAL_FILENAME = 'DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam'
 NORMAL_SAMPLE_SEARCH = '-N0'
-DMP_UNFILTERED_BAM_REGEX = r'-unfilter.bam$'
+DMP_UNFILTERED_REGEX = '-unfilter.bam'
+IGO_UNFILTERED_REGEX = '__aln_srt_IR_FX.bam'
 
 
 def get_request_id_runs(request_id):
@@ -46,8 +48,8 @@ def get_unfiltered_matched_normal(patient_id):
 
     :return: (file_system.models.File - bam, str - sample_id)
     """
-    sample_regex = r'{}.*{}.*__aln_srt_IR_FX.bam$'.format(patient_id, NORMAL_SAMPLE_SEARCH)
-    unfiltered_matched_normal_bam = FileRepository.filter(path_regex=sample_regex)
+    patient_normals_search = patient_id + NORMAL_SAMPLE_SEARCH
+    unfiltered_matched_normal_bam = File.objects.filter(file_name__startswith=patient_normals_search, file_name__endswith=IGO_UNFILTERED_REGEX)
     if len(unfiltered_matched_normal_bam) == 0:
         msg = 'WARNING: Could not find IGO matching unfiltered normal bam file for patient {}. ' \
               'Searching for DMP sample.'
@@ -61,7 +63,7 @@ def get_unfiltered_matched_normal(patient_id):
         query = patient & normal
 
         # Unfiltered bams only
-        file_objs = FileRepository.filter(path_regex=DMP_UNFILTERED_BAM_REGEX)
+        file_objs = FileRepository.filter(path_regex=DMP_UNFILTERED_REGEX)
         unfiltered_matched_normal_bam = FileRepository.filter(queryset=file_objs, q=query)
 
         if len(unfiltered_matched_normal_bam) == 0:
@@ -72,7 +74,7 @@ def get_unfiltered_matched_normal(patient_id):
             unfiltered_matched_normal_bam = None
             unfiltered_matched_normal_sample_id = ''
         else:
-            unfiltered_matched_normal_bam = unfiltered_matched_normal_bam.order_by('-metadata__imported').first()
+            unfiltered_matched_normal_bam = unfiltered_matched_normal_bam.order_by('-metadata__imported').first().file
 
     elif len(unfiltered_matched_normal_bam) > 1:
         msg = 'WARNING: Found more than one matching unfiltered normal bam file for patient {}. ' \
@@ -87,7 +89,7 @@ def get_unfiltered_matched_normal(patient_id):
     # Parse the Normal Sample ID from the file name
     # Todo: Stop using file path for this, once output_metadata is being supplied in access legacy operator
     if unfiltered_matched_normal_bam:
-        unfiltered_matched_normal_file_base = unfiltered_matched_normal_bam.file.path.split('/')[-1]
+        unfiltered_matched_normal_file_base = unfiltered_matched_normal_bam.path.split('/')[-1]
         unfiltered_matched_normal_sample_id = '-'.join(unfiltered_matched_normal_file_base.split('-')[0:3])
 
     return unfiltered_matched_normal_bam, unfiltered_matched_normal_sample_id
--- runner/operator/access/v1_0_0/snps_and_indels/__init__.py
+++ runner/operator/access/v1_0_0/snps_and_indels/__init__.py
@@ -8,12 +8,12 @@ import json
 import logging
 from jinja2 import Template
 
+from file_system.models import File
 from runner.models import Port, RunStatus
 from file_system.models import FileMetadata
 from runner.operator.operator import Operator
-from runner.operator.access import get_request_id_runs, get_unfiltered_matched_normal
 from runner.serializers import APIRunCreateSerializer
-from file_system.repository.file_repository import FileRepository
+from runner.operator.access import get_request_id_runs, get_unfiltered_matched_normal
 
 
 logger = logging.getLogger(__name__)
@@ -22,11 +22,11 @@ WORKDIR = os.path.dirname(os.path.abspath(__file__))
 
 ACCESS_CURATED_BAMS_FILE_GROUP_SLUG = 'access_curated_normals'
 ACCESS_DEFAULT_NORMAL_ID = 'DONOR22-TP'
-ACCESS_DEFAULT_NORMAL_FILENAME = 'DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam$'
+ACCESS_DEFAULT_NORMAL_FILENAME = 'DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam'
 NORMAL_SAMPLE_SEARCH = '-N0'
 TUMOR_SAMPLE_SEARCH = '-L0'
-DUPLEX_BAM_REGEX = r'{}.*__aln_srt_IR_FX-duplex.bam$'
-SIMPLEX_BAM_REGEX = r'{}.*__aln_srt_IR_FX-simplex.bam$'
+DUPLEX_BAM_SEARCH = '__aln_srt_IR_FX-duplex.bam'
+SIMPLEX_BAM_SEARCH = '__aln_srt_IR_FX-simplex.bam'
 
 
 class AccessLegacySNVOperator(Operator):
@@ -81,8 +81,15 @@ class AccessLegacySNVOperator(Operator):
         :return:
         """
         # Locate the most recent Duplex BAM
-        sample_regex = DUPLEX_BAM_REGEX.format(tumor_sample_id)
-        tumor_duplex_bam = FileRepository.filter(path_regex=sample_regex)
+        #
+        # Todo: Does this fail in the following case?
+        # C-000884-L001-d_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam
+        # C-000884-L0011-d_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam
+        #
+        # What if the -d isn't present? Will sample IDs always have these terminators?
+        print(tumor_sample_id)
+        print(DUPLEX_BAM_SEARCH)
+        tumor_duplex_bam = File.objects.filter(file_name__startswith=tumor_sample_id, file_name__endswith=DUPLEX_BAM_SEARCH)
         if len(tumor_duplex_bam) < 1:
             msg = 'ERROR: Could not find matching duplex bam file for sample {}'
             msg = msg.format(tumor_sample_id)
@@ -96,8 +103,7 @@ class AccessLegacySNVOperator(Operator):
         tumor_duplex_bam = tumor_duplex_bam.order_by('-created_date').first()
 
         # Locate the most recent Simplex BAM
-        sample_regex = SIMPLEX_BAM_REGEX.format(tumor_sample_id)
-        tumor_simplex_bam = FileRepository.filter(path_regex=sample_regex)
+        tumor_simplex_bam = File.objects.filter(file_name__startswith=tumor_sample_id, file_name__endswith=SIMPLEX_BAM_SEARCH)
         if len(tumor_simplex_bam) < 1:
             msg = 'ERROR: Could not find matching simplex bam file for sample {}'
             msg = msg.format(tumor_sample_id)
@@ -116,16 +122,15 @@ class AccessLegacySNVOperator(Operator):
         matched_normal_unfiltered_bam, matched_normal_unfiltered_id = get_unfiltered_matched_normal(patient_id)
 
         # Locate any Matched Tumor bams for genotyping
-        matched_duplex_tumor_regex = DUPLEX_BAM_REGEX.format(patient_id + TUMOR_SAMPLE_SEARCH)
-        matched_simplex_tumor_regex = SIMPLEX_BAM_REGEX.format(patient_id + TUMOR_SAMPLE_SEARCH)
-        matched_duplex_tumors = FileRepository.filter(path_regex=matched_duplex_tumor_regex)
-        matched_simplex_tumors = FileRepository.filter(path_regex=matched_simplex_tumor_regex)
+        normal_search = patient_id + TUMOR_SAMPLE_SEARCH
+        matched_duplex_tumors = File.objects.filter(file_name__startswith=normal_search, file_name__endswith=DUPLEX_BAM_SEARCH)
+        matched_simplex_tumors = File.objects.filter(file_name__startswith=normal_search, file_name__endswith=SIMPLEX_BAM_SEARCH)
         # Remove the main tumor being run
-        matched_duplex_tumors = matched_duplex_tumors.exclude(file__file_name=tumor_duplex_bam.file.file_name)
-        matched_simplex_tumors = matched_simplex_tumors.exclude(file__file_name=tumor_simplex_bam.file.file_name)
-        matched_duplex_sample_ids = ['-'.join(b.file.path.split('/')[-1].split('-')[0:3]) for b in
+        matched_duplex_tumors = matched_duplex_tumors.exclude(file_name=tumor_duplex_bam.file_name)
+        matched_simplex_tumors = matched_simplex_tumors.exclude(file_name=tumor_simplex_bam.file_name)
+        matched_duplex_sample_ids = ['-'.join(b.path.split('/')[-1].split('-')[0:3]) for b in
                                      matched_duplex_tumors]
-        matched_simplex_sample_ids = ['-'.join(b.file.path.split('/')[-1].split('-')[0:3]) for b in
+        matched_simplex_sample_ids = ['-'.join(b.path.split('/')[-1].split('-')[0:3]) for b in
                                       matched_simplex_tumors]
 
         sample_info = {
@@ -177,12 +182,12 @@ class AccessLegacySNVOperator(Operator):
         curated_normals_metadata = FileMetadata.objects.filter(
             file__file_group__slug=ACCESS_CURATED_BAMS_FILE_GROUP_SLUG
         )
-        curated_normal_bams = [f.file for f in curated_normals_metadata]
+        curated_normal_bams = [f for f in curated_normals_metadata]
         curated_normal_ids = [f.metadata['snv_pipeline_id'] for f in curated_normals_metadata]
         normal_bams = [
             {
                 'class': 'File',
-                'location': 'juno://' + b.path
+                'location': 'juno://' + b.file.path
             } for b in curated_normal_bams
         ]
         return normal_bams, curated_normal_ids
@@ -201,15 +206,15 @@ class AccessLegacySNVOperator(Operator):
             tumor_sample_names = [tumor_sample_id]
             tumor_bams = [{
                 "class": "File",
-                "location": 'juno://' + tumor_duplex_bam.file.path
+                "location": 'juno://' + tumor_duplex_bam.path
             }]
             matched_normal_ids = [matched_normal_unfiltered_id]
 
             # Todo: how to know which sequencer's default normal to use?
-            normal_bam = FileRepository.filter(
-                file_type='bam',
-                path_regex=ACCESS_DEFAULT_NORMAL_FILENAME
-            )[0].file
+            normal_bam = File.objects.filter(
+                file_type__name='bam',
+                file_name=ACCESS_DEFAULT_NORMAL_FILENAME
+            )[0]
             normal_bams = [{
                 "class": "File",
                 "location": 'juno://' + normal_bam.path
@@ -219,11 +224,11 @@ class AccessLegacySNVOperator(Operator):
             genotyping_bams = [
                 {
                     "class": "File",
-                    "location": 'juno://' + tumor_duplex_bam.file.path
+                    "location": 'juno://' + tumor_duplex_bam.path
                 },
                 {
                     "class": "File",
-                    "location": 'juno://' + tumor_simplex_bam.file.path
+                    "location": 'juno://' + tumor_simplex_bam.path
                 }
             ]
 
@@ -233,7 +238,7 @@ class AccessLegacySNVOperator(Operator):
             if matched_normal_unfiltered:
                 genotyping_bams += [{
                     "class": "File",
-                    "location": 'juno://' + matched_normal_unfiltered.file.path
+                    "location": 'juno://' + matched_normal_unfiltered.path
                 }]
                 genotyping_bams_ids += [matched_normal_unfiltered_id]
 
@@ -242,7 +247,7 @@ class AccessLegacySNVOperator(Operator):
                 genotyping_bams += [
                     {
                         "class": "File",
-                        "location": 'juno://' + b.file.path
+                        "location": 'juno://' + b.path
                     } for b in matched_tumors_duplex
                 ]
                 genotyping_bams_ids += matched_tumors_duplex_sample_ids
@@ -250,7 +255,7 @@ class AccessLegacySNVOperator(Operator):
                 genotyping_bams += [
                     {
                         "class": "File",
-                        "location": 'juno://' + b.file.path
+                        "location": 'juno://' + b.path
                     } for b in matched_tumors_simplex
                 ]
                 genotyping_bams_ids += [i + '-SIMPLEX' for i in matched_tumors_simplex_sample_ids]
