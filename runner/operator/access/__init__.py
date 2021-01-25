403d2d0975f79a9458e318dc9f3f1afbd87e75d0
Ian <ionox0@gmail.com>
2021-01-25 17:39:57 -0500

    ACCESS - genotyping matched tumors (#678)
    * initial commit for genotyping matched tumors
    
    * clean up fixtures
    
    * refactor into separate functions
    
    * add test for matched Tumor bam inputs
    
    * Use startswith and endswith to avoid regex and speed up File queries
    
    * deduplicate when multiple runs of same bam are present
    
    * Use FileMetadata.filter() as providing a Q object to FileRepository.filter() does not operate on the given queryset
    
    * fix comment
    
    * clarify comment

--- runner/operator/access/__init__.py
+++ runner/operator/access/__init__.py
@@ -1,17 +1,17 @@
 import logging
 
 from runner.models import Run, RunStatus
-from django.db.models import Prefetch, Q
-from file_system.repository.file_repository import FileRepository
+from file_system.models import File, FileMetadata
 
 
 logger = logging.getLogger(__name__)
 
 ACCESS_CURATED_BAMS_FILE_GROUP_SLUG = 'access_curated_normals'
 ACCESS_DEFAULT_NORMAL_ID = 'DONOR22-TP'
-ACCESS_DEFAULT_NORMAL_FILENAME = r'DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam$'
+ACCESS_DEFAULT_NORMAL_FILENAME = 'DONOR22-TP_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam'
 NORMAL_SAMPLE_SEARCH = '-N0'
-DMP_UNFILTERED_BAM_REGEX = r'-unfilter.bam$'
+DMP_UNFILTERED_REGEX = '-unfilter.bam'
+IGO_UNFILTERED_REGEX = '__aln_srt_IR_FX.bam'
 
 
 def get_request_id_runs(request_id):
@@ -46,33 +46,30 @@ def get_unfiltered_matched_normal(patient_id):
 
     :return: (file_system.models.File - bam, str - sample_id)
     """
-    sample_regex = r'{}.*{}.*__aln_srt_IR_FX.bam$'.format(patient_id, NORMAL_SAMPLE_SEARCH)
-    unfiltered_matched_normal_bam = FileRepository.filter(path_regex=sample_regex)
+    patient_normals_search = patient_id + NORMAL_SAMPLE_SEARCH
+    unfiltered_matched_normal_bam = File.objects.filter(file_name__startswith=patient_normals_search, file_name__endswith=IGO_UNFILTERED_REGEX)
     if len(unfiltered_matched_normal_bam) == 0:
         msg = 'WARNING: Could not find IGO matching unfiltered normal bam file for patient {}. ' \
               'Searching for DMP sample.'
         msg = msg.format(patient_id)
         logger.warning(msg)
 
-        # Todo: need to specify ACCESS as assay?
-        # assay = Q(metadata__cmo_assay=value)
-        patient = Q(metadata__patient__cmo=patient_id.replace('C-', ''))
-        normal = Q(metadata__type='N')
-        query = patient & normal
-
-        # Unfiltered bams only
-        file_objs = FileRepository.filter(path_regex=DMP_UNFILTERED_BAM_REGEX)
-        unfiltered_matched_normal_bam = FileRepository.filter(queryset=file_objs, q=query)
+        unfiltered_matched_normal_bam = FileMetadata.objects.filter(
+            metadata__cmo_assay='ACCESS_V1_0',
+            metadata__patient__cmo=patient_id.replace('C-', ''),
+            metadata__type='N',
+            file__path__endswith=DMP_UNFILTERED_REGEX
+        )
 
         if len(unfiltered_matched_normal_bam) == 0:
             msg = 'WARNING: Could not find DMP or IGO matching unfiltered normal bam file for patient {}. ' \
-                  'We will skip running this sample.'
+                  'We will skip matched normal genotyping for this sample.'
             msg = msg.format(patient_id)
             logger.warning(msg)
             unfiltered_matched_normal_bam = None
             unfiltered_matched_normal_sample_id = ''
         else:
-            unfiltered_matched_normal_bam = unfiltered_matched_normal_bam.order_by('-metadata__imported').first()
+            unfiltered_matched_normal_bam = unfiltered_matched_normal_bam.order_by('-metadata__imported').first().file
 
     elif len(unfiltered_matched_normal_bam) > 1:
         msg = 'WARNING: Found more than one matching unfiltered normal bam file for patient {}. ' \
@@ -87,7 +84,7 @@ def get_unfiltered_matched_normal(patient_id):
     # Parse the Normal Sample ID from the file name
     # Todo: Stop using file path for this, once output_metadata is being supplied in access legacy operator
     if unfiltered_matched_normal_bam:
-        unfiltered_matched_normal_file_base = unfiltered_matched_normal_bam.file.path.split('/')[-1]
+        unfiltered_matched_normal_file_base = unfiltered_matched_normal_bam.path.split('/')[-1]
         unfiltered_matched_normal_sample_id = '-'.join(unfiltered_matched_normal_file_base.split('-')[0:3])
 
     return unfiltered_matched_normal_bam, unfiltered_matched_normal_sample_id
