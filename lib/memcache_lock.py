bb02d26ce1874dc6ae78a39c6ee6ef80fb9fd120
Adrian Fraiha <aef@fastmail.com>
2021-02-04 23:23:19 -0500

    remove self

--- lib/memcache_lock.py
+++ lib/memcache_lock.py
@@ -5,13 +5,13 @@ from django.core.cache import cache
 def memcache_lock(lock_id, expiration = 60 * 10):
     def decorator(func):
         @functools.wraps(func)
-        def wrapper(self, *args, **kwargs):
+        def wrapper(*args, **kwargs):
             timeout_at = time.monotonic() + expiration - 3
 
             acquired = cache.add(lock_id, 1, expiration)
             if acquired:
                 try:
-                    func(self, *args, **kwargs)
+                    func(*args, **kwargs)
                 finally:
                     if time.monotonic() < timeout_at:
                         cache.delete(lock_id)
