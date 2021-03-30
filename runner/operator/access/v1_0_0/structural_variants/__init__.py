330d75d9eec1aec983aae41dcbf2b0416ad4f575
Ian <ionox0@gmail.com>
2021-03-30 10:21:45 -0400

    refactor Tumor or Normal searching into common function, and allow anything besides "-N" to count as tumor (#781)

--- runner/operator/access/v1_0_0/structural_variants/__init__.py
+++ runner/operator/access/v1_0_0/structural_variants/__init__.py
@@ -4,7 +4,7 @@ import logging
 from jinja2 import Template
 
 from runner.operator.operator import Operator
-from runner.operator.access import get_request_id_runs, get_request_id
+from runner.operator.access import get_request_id, get_request_id_runs, extract_tumor_ports
 from runner.serializers import APIRunCreateSerializer
 from file_system.repository.file_repository import File
 from runner.models import Port, RunStatus
@@ -44,9 +44,8 @@ class AccessLegacySVOperator(Operator):
         )
 
         # Filter to only tumor bam files
-        # Todo: Use separate metadata fields for Tumor / sample ID designation instead of file name
-        standard_tumor_bam_files = [f for p in standard_bam_ports for f in p.value if TUMOR_OR_NORMAL_SEARCH in f['location'].split('/')[-1]]
-        sample_ids = [f['location'].split('/')[-1].split(SAMPLE_ID_SEP)[0] for f in standard_tumor_bam_files]
+        standard_tumor_ports = extract_tumor_ports(standard_bam_ports)
+        sample_ids = [f['location'].split('/')[-1].split(SAMPLE_ID_SEP)[0] for f in standard_tumor_ports]
 
         normal_bam = File.objects.filter(file_name=ACCESS_DEFAULT_SV_NORMAL_FILENAME)
         if not len(normal_bam) == 1:
@@ -56,7 +55,7 @@ class AccessLegacySVOperator(Operator):
         normal_bam = normal_bam[0]
 
         sample_inputs = []
-        for i, b in enumerate(standard_tumor_bam_files):
+        for i, b in enumerate(standard_tumor_ports):
             sample_input = self.construct_sample_inputs(
                 sample_ids[i],
                 b,
