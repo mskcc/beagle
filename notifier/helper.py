4886c7d0e8e8be9389f812f68ed25adc4315844d
(Christopher) Allan Bolipata <bolipatc@mskcc.org>
2021-04-21 18:29:08 -0400

    Add manifest gen init commit for helper.py

--- notifier/helper.py
+++ notifier/helper.py
@@ -38,3 +38,57 @@ def generate_sample_data_content(files, pipeline_name, pipeline_github, pipeline
             pipeline_version,
         )
     return result
+
+
+def generate_json_manifest(request_id):
+    files = FileRepository.objects.all()
+    q = Q(metadata__requestId=request_id)
+    q_fg = Q(file__file_group_id=settings.IMPORT_FILE_GROUP)
+    q_fg |= Q(file__file_group__slug="origin-unknown")
+    ret_str = 'metadata__sampleId'
+    query = q & q_fg & Q(file__path__in=files)
+    samples = FileRepository.filter(q=query).order_by(ret_str).distinct(ret_str).all()
+    request_data = request_keys()
+    samples_list = list()
+    for sample in samples:
+        metadata = sample.metadata
+        for key in request_data:
+            if not request_data[key]: # if request_data[key] hasn't been filled yet
+                request_data[key] = get_data(metadata, key)
+        sample_data = sample_keys()
+        for key in sample_data:
+            if key in metadata:
+                sample_data[key] = get_data(metadata, key)
+        samples_list.append(sample_data)
+    data = dict()
+    data['request'] = request_data
+    data['samples'] = samples_list
+    return data
+
+
+def get_data(metadata, k):
+    if k in metadata:
+        return MetadataValidator.clean_value(metadata[k])
+    return ''
+
+
+def sample_keys():
+    keys = [ 'cmoSampleName', 'patientId', 'investigatorSampleId', 'sampleClass',
+            'oncoTreeCode', 'specimenType', 'preservation', 'sex', 'tissueLocation',
+            'sampleId', 'runMode' ]
+    r = dict()
+    for key in keys:
+        r[key] = ""
+    return r
+
+
+def request_keys():
+    keys = [ 'dataAnalystEmail', 'dataAccessEmails', 'dataAnalystName',
+            'investigatorEmail', 'labHeadEmail', 'labHeadName', 'libraryType',
+            'otherContactEmails', 'piEmail', 'recipe', 'projectManagerName',
+            'qcAccessEmails', 'investigatorName' ]
+
+    r = dict()
+    for key in keys:
+        r[key] = ''
+    return r
