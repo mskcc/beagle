674e7159ba7526ec94739915493b08709d1a7053
Adrian Fraiha <aef@fastmail.com>
2021-05-19 17:00:15 -0400

    operator to generate inputs for sample sheets

---
+++ runner/operator/access/v1_0_0/sample_sheet/__init__.py
@@ -0,0 +1,66 @@
+import os
+import json
+import logging
+from jinja2 import Template
+
+from file_system.repository.file_repository import FileRepository
+from runner.operator.operator import Operator
+from runner.operator.access import get_request_id, get_request_id_runs, extract_tumor_ports
+from runner.models import Port, RunStatus
+from runner.serializers import APIRunCreateSerializer
+
+
+logger = logging.getLogger(__name__)
+
+class AccessSampleSheetOperator(Operator):
+    def get_jobs(self):
+        """
+        Convert job inputs into serialized jobs
+
+        :return: list[(serialized job info, Job)]
+        """
+        files = FileRepository.filter(queryset=self.files,
+                                      metadata={'requestId': self.request_id,
+                                                'igocomplete': True})
+
+        samples = []
+        for f in files:
+            if f.metadata["barcodeIndex"]:
+                barcodeIndex = f.metadata["barcodeIndex"].split("-")
+                index1 = barcodeIndex[0]
+                if len(barcodeIndex) > 1:
+                    index2 = barcodeIndex[1]
+
+            for lane in f.metadata["flowCellLanes"]:
+                samples.append({
+                    "lane": lane,
+                    "sample_id": f.metadata["sampleId"],
+                    "sample_plate": f.metadata["tumorOrNormal"],
+                    "sample_well": f.metadata["recipe"],
+                    "17_index_id": f.metadata["barcodeId"],
+                    "index": index1,
+                    "index2": index2,
+                    "sample_project": "Project_" + self.request_id,
+                    "description": f.metadata["dataAnalystName"]
+                })
+
+        inputs = [{
+            "samples": samples
+        }]
+
+        return [
+            (
+                APIRunCreateSerializer(
+                    data={
+                        'name': "Sample Sheet: %s, %i of %i" % (self.request_id, i + 1, len(inputs)),
+                        'app': self.get_pipeline_id(),
+                        'inputs': job,
+                        'tags': {
+                            'requestId': self.request_id
+                        }
+                    }
+                ),
+                job
+             )
+            for i, job in enumerate(inputs)
+        ]
