a40d179ff7d07707238876b83a3c9a7ea7cdb4fb
Allan Bolipata <allan.bolipata@gmail.com>
2021-05-20 23:37:22 -0700

    Add group owner settings, recursive group owner change in copy service

--- beagle_etl/copy_service/copy_service.py
+++ beagle_etl/copy_service/copy_service.py
@@ -8,23 +8,34 @@ logger = logging.getLogger(__name__)
 
 
 class CopyService(object):
-
     @staticmethod
     def copy(path_from, path_to):
-        logger.info('Copy path from {path_from} to {path_to}'.format(path_from=path_from,
-                                                                     path_to=path_to))
+        logger.info(
+            "Copy path from {path_from} to {path_to}".format(
+                path_from=path_from, path_to=path_to
+            )
+        )
         dirname = os.path.dirname(path_to)
         if not os.path.exists(dirname):
             os.makedirs(dirname, mode=settings.COPY_DIR_PERMISSION)
         copyfile(path_from, path_to)
         os.chmod(path_to, settings.COPY_FILE_PERMISSION)
 
+        if settings.COPY_GROUP_OWNERSHIP:
+            for dirpath, dirnames, filenames in os.walk(path_to):
+                shutil.chown(dirpath, group=settings.COPY_GROUP_OWNERSHIP)
+                for filename in filenames:
+                    shutil.chown(
+                        os.path.join(dirpath, filename),
+                        group=settings.COPY_GROUP_OWNERSHIP,
+                    )
+
     @staticmethod
     def remap(recipe, path, mapping=settings.DEFAULT_MAPPING):
         prefix, dst = CopyService.get_mapping(recipe, path, mapping)
         if prefix and dst:
             path = path.replace(prefix, dst)
-        logger.info('New path {path}'.format(path=path))
+        logger.info("New path {path}".format(path=path))
         return path
 
     @staticmethod
