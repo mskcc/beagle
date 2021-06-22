5bd82172893a419ab70273e2080dcdf86faea7b5
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2021-06-22 07:13:54 -0400

    Configuration to connect to metadb

--- beagle_etl/nats_client/nats_client.py
+++ beagle_etl/nats_client/nats_client.py
@@ -1,58 +1,73 @@
+import ssl
 import json
 import signal
+import logging
 import asyncio
-from nats.aio.client import Client as NATS
+from django.conf import settings
 from beagle_etl.tasks import TYPES
 from beagle_etl.models import Job, JobStatus
+from nats.aio.client import Client as NATS
+
+
+logger = logging.getLogger(__name__)
+
 
 async def run(loop):
     nc = NATS()
 
     async def error_cb(e):
-        print("Error:", e)
+        logger.error("Error:", e)
 
     async def closed_cb():
-        print("Connection to NATS is closed.")
+        logger.info("Connection to NATS is closed.")
         await asyncio.sleep(0.1, loop=loop)
         loop.stop()
 
     async def reconnected_cb():
-        print(f"Connected to NATS at {nc.connected_url.netloc}...")
+        logger.info(f"Connected to NATS at {nc.connected_url.netloc}...")
 
     async def subscribe_handler(msg):
         subject = msg.subject
         reply = msg.reply
         data = msg.data.decode()
         request_data = json.loads(data)
-        print("Received a message on '{subject} {reply}': {data}".format(
+
+        logger.info("Received a message on '{subject} {reply}': {data}".format(
           subject=subject, reply=reply, data=data))
         job = Job(run=TYPES['PARSE_NEW_REQUEST'], args={'request': request_data},
                   status=JobStatus.CREATED,
                   max_retry=3, children=[])
         job.save()
 
+    ssl_ctx = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+    ssl_ctx.load_cert_chain(certfile=settings.NATS_SSL_CERTFILE,
+                            keyfile=settings.NATS_SSL_KEYFILE)
+
     options = {
         "loop": loop,
         "error_cb": error_cb,
         "closed_cb": closed_cb,
-        "reconnected_cb": reconnected_cb
+        "reconnected_cb": reconnected_cb,
+        "servers": settings.METADB_NATS_URL,
+        "username": settings.METADB_USERNAME,
+        "password": settings.METADB_PASSWORD,
+        "tls": ssl_ctx
     }
 
     try:
-        options['servers'] = "nats://127.0.0.1:4222"
         await nc.connect(**options)
     except Exception as e:
-        print(e)
+        logger.error(e)
 
-    print(f"Connected to NATS at {nc.connected_url.netloc}...")
+    logger.info(f"Connected to NATS at {nc.connected_url.netloc}...")
 
     def signal_handler():
         if nc.is_closed:
             return
-        print("Disconnecting...")
+        logger.info("Disconnecting...")
         loop.create_task(nc.close())
 
     for sig in ('SIGINT', 'SIGTERM'):
         loop.add_signal_handler(getattr(signal, sig), signal_handler)
 
-    await nc.subscribe('NEW_REQUEST', "", subscribe_handler)
+    await nc.subscribe(settings.METADB_NATS_SUBJECT, "", subscribe_handler)
