b5f56821c2f0ebc355be8a54f2af5b1f5476c96e
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2021-09-24 15:06:10 -0400

    Fix serializer

--- runner/serializers.py
+++ runner/serializers.py
@@ -379,9 +379,12 @@ class PairOperatorSerializer(serializers.Serializer):
     name = serializers.CharField(allow_blank=False, allow_null=False)
     output_directory_prefix = serializers.CharField(max_length=50, allow_blank=True, allow_null=True)
     job_group_id = serializers.UUIDField(required=False)
-    labHeadName = serializers.CharField(max_length=30, required=False, allow_blank=True, allow_null=True)
-    investigatorName = serializers.CharField(max_length=30, required=False, allow_blank=False, allow_null=False)
-    assay = serializers.CharField(max_length=30, required=False, allow_blank=False, allow_null=False)
+    labHeadName = serializers.CharField(max_length=30, required=False,
+                                        allow_blank=True, allow_null=True)
+    investigatorName = serializers.CharField(max_length=30, required=False,
+                                             allow_blank=True, allow_null=True)
+    assay = serializers.CharField(max_length=30, required=False,
+                                  allow_blank=True, allow_null=True)
 
 
 class OperatorErrorSerializer(serializers.ModelSerializer):
