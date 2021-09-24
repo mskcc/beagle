8ba67f48c926d17af2eca4123eabe36f047f0d3e
Nikhil Kumar <nikhilkumar516@gmail.com>
2021-09-24 14:54:59 -0400

    Updated and fixed the ultron operator

--- runner/operator/ultron/v1_0_0/phase1/ultron_operator.py
+++ runner/operator/ultron/v1_0_0/phase1/ultron_operator.py
@@ -13,9 +13,22 @@ from runner.operator.operator import Operator
 from runner.serializers import APIRunCreateSerializer
 from runner.models import Pipeline
 from file_system.repository.file_repository import FileRepository
+from file_system.models import FileGroup
 import json
 WORKDIR = os.path.dirname(os.path.abspath(__file__))
 LOGGER = logging.getLogger(__name__)
+
+
+def get_file_group(file_group_name):
+    file_group_obj = FileGroup.objects.get(name=file_group_name)
+    if not file_group_obj:
+        LOGGER.error("No file group found for %s", file_group_name)
+        return None
+    if type(file_group_obj) == list and len(file_group_obj) > 1:
+        logging.error("More than one file group returned for %s",
+                      file_group_name)
+        return None
+    return file_group_obj.pk
 
 
 class UltronOperator(Operator):
@@ -35,22 +48,20 @@ class UltronOperator(Operator):
 
         return ultron_output_jobs
 
-
     def _build_inputs(self, run_ids):
-        run_jsons = list() 
+        run_jsons = list()
         for rid in set(run_ids):
             run = Run.objects.filter(id=rid)[0]
             inputs_data = InputsObj(run)
             run_jsons.append(inputs_data.inputs_json)
         return run_jsons
 
-
     def _build_job(self, input_json):
         app = self.get_pipeline_id()
         pipeline = Pipeline.objects.get(id=app)
         pipeline_version = pipeline.version
-        sample_name = input_json['sample_names'][0] # should only be one
-        tags = { 'sampleNameTumor': sample_name }
+        sample_name = input_json['sample_names'][0]  # should only be one
+        tags = {'sampleNameTumor': sample_name}
         # add tags, name
         output_job_data = {
             'app': app,
@@ -70,7 +81,7 @@ class InputsObj:
     # provided they exist
     def __init__(self, run):
         self.run = run
-        self.port_list = Port.objects.filter(run = run.id)
+        self.port_list = Port.objects.filter(run=run.id)
         self.tumor_sample_name = run.tags['sampleNameTumor']
         self.sample = self._get_samples_data()
         self.tumor_bam = self._get_port("tumor_bam")
@@ -81,13 +92,12 @@ class InputsObj:
         self.inputs_data = self._set_inputs_data()
         self.inputs_json = self._build_inputs_json()
 
-
     def _get_samples_data(self):
         files = FileRepository.all()
         f = FileRepository.filter(queryset=files,
-                metadata={'cmoSampleName': self.tumor_sample_name,
-                    'igocomplete': True},
-                filter_redact=True)
+                                  metadata={'cmoSampleName': self.tumor_sample_name,
+                                            'igocomplete': True},
+                                  filter_redact=True)
         sample = None
         if f:
             # retrieve metadata from first record (should only be one)
@@ -96,14 +106,12 @@ class InputsObj:
             sample = SampleData(sample_id)
         return sample
 
-
     def _get_port(self, port_name):
         for single_port in self.port_list:
             current_port = single_port.name
             if current_port == port_name:
-                 return self._get_files_from_port(single_port.value)
+                return self._get_files_from_port(single_port.value)
         return None
-
 
     def _get_files_from_port(self, port_obj):
         file_list = []
@@ -114,8 +122,7 @@ class InputsObj:
             file_list.append(self._get_file_obj(port_obj))
         return file_list
 
-
-    def _get_file_obj(self,file_obj):
+    def _get_file_obj(self, file_obj):
         """
         Given file_obj, construct a dictionary of class File, that file's
         JUNO-specific URI file path, and a list of secondary files with
@@ -135,11 +142,9 @@ class InputsObj:
             file_cwl_obj['secondaryFiles'] = secondary_file_list
         return file_cwl_obj
 
-
     def _create_cwl_file_obj(self, file_path):
         cwl_file_obj = {'class': 'File', 'location': "juno://%s" % file_path}
         return cwl_file_obj
-
 
     def _set_inputs_data(self):
         sample = self.sample
@@ -154,29 +159,40 @@ class InputsObj:
         inputs_data['tumor_sample_name'].append(sample.cmo_sample_name)
         if sample.dmp_bams_tumor:
             for f in sample.dmp_bams_tumor:
-                inputs_data['dmp_bams_tumor'].append(self._create_cwl_file_obj(f.bam_path))
-                inputs_data['dmp_bams_tumor_sample_name'].append(f.dmp_sample_name)
+                inputs_data['dmp_bams_tumor'].append(
+                    self._create_cwl_file_obj(f.bam_path))
+                inputs_data['dmp_bams_tumor_sample_name'].append(
+                    f.dmp_sample_name)
                 if f.mutations_extended:
-                    inputs_data['dmp_bams_tumor_muts'].append(self._create_cwl_file_obj(f.mutations_extended))
+                    inputs_data['dmp_bams_tumor_muts'].append(
+                        self._create_cwl_file_obj(f.mutations_extended))
         return inputs_data
-
 
     def _build_inputs_json(self):
         inputs_json = dict()
-        inputs_json['dmp_bams'] = self.inputs_data['dmp_bams_tumor']
-        inputs_json['dmp_bams_sample_names'] = self.inputs_data['dmp_bams_tumor_sample_name']
-        inputs_json['dmp_maf_files'] = self.inputs_data['dmp_bams_tumor_muts']
+        inputs_json['unindexed_bam_files'] = self.inputs_data['dmp_bams_tumor']
+        inputs_json['unindexed_sample_ids'] = self.inputs_data['dmp_bams_tumor_sample_name']
+        inputs_json['unindexed_maf_files'] = self.inputs_data['dmp_bams_tumor_muts']
         inputs_json['maf_files'] = self.inputs_data['maf']
         inputs_json['bam_files'] = self.inputs_data['tumor_bam']
-        inputs_json['sample_names'] = self.inputs_data['tumor_sample_name']
+        inputs_json['sample_ids'] = self.inputs_data['tumor_sample_name']
         inputs_json['ref_fasta'] = self.load_reference_fasta()
+        inputs_json['exac_filter'] = self.load_exac_filter()
         return inputs_json
 
-
     def load_reference_fasta(self):
-        ref_fasta_path = json.load(open(os.path.join(WORKDIR, "reference_json/genomic_resources.json"), 'rb'))
-        ref_fasta = {"class": "File", "location": str(ref_fasta_path['ref_fasta']) }
+        ref_fasta_path = json.load(
+            open(os.path.join(WORKDIR, "reference_json/genomic_resources.json"), 'rb'))
+        ref_fasta = {"class": "File", "location": str(
+            ref_fasta_path['ref_fasta'])}
         return ref_fasta
+
+    def load_exac_filter(self):
+        exac_filter_path = json.load(
+            open(os.path.join(WORKDIR, "reference_json/genomic_resources.json"), 'rb'))
+        exac_filter = {"class": "File", "location": str(
+            exac_filter_path['exac_filter'])}
+        return exac_filter
 
 
 class SampleData:
@@ -185,6 +201,7 @@ class SampleData:
     def __init__(self, sample_id):
         self.files = FileRepository.all()
         self.sample_id = sample_id
+        self.dmp_bam_file_group = get_file_group("DMP BAMs")
         self.patient_id, self.cmo_sample_name = self._get_sample_metadata()
         self.dmp_patient_id = self._get_dmp_patient_id()
         self.dmp_bams_tumor = self._find_dmp_bams("T")
@@ -193,10 +210,9 @@ class SampleData:
     def _get_sample_metadata(self):
         # gets patient id and cmo sample name from sample id query
         # condensed in this one  fucntion to reduce amount of queriesd
-        files = FileRepository.filter(queryset=self.files,
-                metadata={'sampleId': self.sample_id,
-                    'igocomplete': True},
-                filter_redact=True)
+        files = FileRepository.filter(metadata={'sampleId': self.sample_id,
+                                                'igocomplete': True},
+                                      filter_redact=True)
         # there should only be one patient ID
         # looping through the metadata works here, but it's lazy
         patient_id = None
@@ -213,7 +229,6 @@ class SampleData:
                     sample_name = sid
         return patient_id, sample_name
 
-
     def _get_dmp_patient_id(self):
         # Remove C- prefix to match DMP patient ID format
         if self.patient_id:
@@ -224,8 +239,8 @@ class SampleData:
         # Retrieves dmp samples based on dmp bams
         file_list = list()
         if self.dmp_patient_id:
-            files = FileRepository.filter(queryset=self.files,
-                    metadata={'patient__cmo': self.dmp_patient_id, "type": tumor_type})
+            files = FileRepository.filter(queryset=self.files, file_group=self.dmp_bam_file_group,
+                                          metadata={'patient__cmo': self.dmp_patient_id, "type": tumor_type})
             if files:
                 for f in files:
                     file_list.append(BamData(f))
@@ -235,8 +250,9 @@ class SampleData:
     def __str__(self):
         return "Sample ID: %s ; Patient ID: %s ;\
                 DMP Patient ID: %s" % (self.sample_id,
-                        self.patient_id, self.dmp_patient_id)
+                                       self.patient_id, self.dmp_patient_id)
         return results
+
 
 class BamData:
     def __init__(self, dmp_file):
@@ -244,29 +260,26 @@ class BamData:
         self.dmp_file = dmp_file
         self.bam_path = dmp_file.file.path
         self.metadata = dmp_file.metadata
+        self.mutations_extended_file_group = get_file_group(
+            "DMP Data Mutations Extended")
         self.mutations_extended = self._set_data_muts_txt()
         self.dmp_sample_name = self._set_dmp_sample_name()
 
     def _set_data_muts_txt(self):
-        dmp_data_id = self._get_dmp_sample_id()
-        results = self._get_muts(dmp_data_id)
+        results = self._get_muts()
         return results
 
-    def _get_dmp_sample_id(self):
-        meta = self.metadata
-        dmp_key = "sample"
-        data_id = meta[dmp_key]
-        return data_id
-
-    def _get_muts(self, data_id):
+    def _get_muts(self):
         # There should only be one mutations file returned here, one per dmp sample
-        query_results = FileRepository.filter(queryset=self.files, metadata={'dmp_link_id':data_id})
+        query_results = FileRepository.filter(
+            queryset=self.files, file_group=self.mutations_extended_file_group, metadata={'sample': self.metadata['sample']})
         results = list()
         if query_results:
             for item in query_results:
                 results.append(item.file.path)
         if len(results) > 1:
-            LOGGER.error("More than one mutations file found for %s", data_id)
+            LOGGER.error("More than one mutations file found for %s",
+                         self.metadata['sample'])
         if results:
             return results[0]
         return results
