ff6ef2cce7b361f88699d130db391e9aded5f433
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2021-12-07 08:40:17 -0500

    container fixes and remove unused imports

--- beagle_etl/tasks.py
+++ beagle_etl/tasks.py
@@ -3,17 +3,15 @@ import importlib
 import datetime
 import asyncio
 from celery import shared_task
+from lib.logger import format_log
 from django.conf import settings
 from beagle_etl.models import JobStatus, Job
 from beagle_etl.jobs.lims_etl_jobs import TYPES
 from beagle_etl.exceptions import ETLExceptions
+from beagle_etl.nats_client.nats_client import run
 from file_system.repository import FileRepository
 from notifier.tasks import send_notification
-from notifier.events import PermissionDeniedEvent, SendEmailEvent
-from notifier.events import ETLImportEvent, ETLJobsLinksEvent
-from beagle_etl.nats_client.nats_client import run
-from notifier.events import ETLImportEvent, ETLJobsLinksEvent, ETLJobFailedEvent, PermissionDeniedEvent, SendEmailEvent
-from lib.logger import format_log
+from notifier.events import ETLImportEvent, ETLJobsLinksEvent, PermissionDeniedEvent, SendEmailEvent
 
 
 logger = logging.getLogger(__name__)
