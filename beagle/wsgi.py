c63c9d9435623d9b38d083b3749f73be3636dfb4
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2022-01-03 13:29:01 -0500

    WSGI remove unused imports

--- beagle/wsgi.py
+++ beagle/wsgi.py
@@ -10,8 +10,6 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-from beagle_etl.tasks import fetch_request_nats
-from concurrent.futures import ThreadPoolExecutor, wait
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beagle.settings')
 
