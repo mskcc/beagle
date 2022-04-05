37fb91c3b010ea02a1ef4f3ff0edf4bf6fbb60fa
Nikhil Kumar <nikhilkumar516@gmail.com>
2022-04-05 13:37:09 -0400

    Set metadata keys to settings values

--- runner/operator/access/v2_1_0/nucleo/nucleo_operator.py
+++ runner/operator/access/v2_1_0/nucleo/nucleo_operator.py
@@ -132,7 +132,7 @@ class AccessNucleoOperator(Operator):
                     "app": self.get_pipeline_id(),
                     "inputs": job,
                     "output_metadata": {key: metadata[key] for key in METADATA_OUTPUT_FIELDS if key in metadata},
-                    "tags": {settings.REQUEST_ID_METADATA_KEY: self.request_id, "cmoSampleId": metadata["sampleName"]},
+                    "tags": {settings.REQUEST_ID_METADATA_KEY: self.request_id, "cmoSampleId": metadata[settings.SAMPLE_NAME_METADATA_KEY]},
                 }
             )
             for i, (job, metadata) in enumerate(sample_inputs)
