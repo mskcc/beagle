4b2b7c99314f67bfa36b42e9a642f982ff332d36
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2022-05-16 09:54:39 -0400

    request callback delay

--- beagle_etl/celery.py
+++ beagle_etl/celery.py
@@ -56,9 +56,15 @@ app.conf.task_routes = {
     "beagle_etl.jobs.metadb_jobs.update_request_job": {"queue": settings.BEAGLE_DEFAULT_QUEUE},
     "beagle_etl.jobs.metadb_jobs.update_sample_job": {"queue": settings.BEAGLE_DEFAULT_QUEUE},
     "beagle_etl.jobs.metadb_jobs.not_supported": {"queue": settings.BEAGLE_DEFAULT_QUEUE},
+    "beagle_etl.jobs.metadb_jobs.request_callback": {"queue": settings.BEAGLE_DEFAULT_QUEUE},
 }
 
 app.conf.beat_schedule = {
+    "process_request_callback_jobs": {
+        "task": "beagle_etl.tasks.process_request_callback_jobs",
+        "schedule": settings.PROCESS_SMILE_MESSAGES_PERIOD,
+        "options": {"queue": settings.BEAGLE_DEFAULT_QUEUE},
+    },
     "process_smile_imports": {
         "task": "beagle_etl.tasks.process_smile_events",
         "schedule": settings.PROCESS_SMILE_MESSAGES_PERIOD,
