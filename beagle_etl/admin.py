7bcd288a6443a50c68fe98e2d0b4a42aad364097
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2022-05-26 09:30:27 -0400

    Store metadata schema in DB

--- beagle_etl/admin.py
+++ beagle_etl/admin.py
@@ -3,7 +3,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin, SimpleListFilter
 from django.utils.safestring import mark_safe
-from .models import Job, JobStatus, Operator, ETLConfiguration, SMILEMessage, RequestCallbackJob
+from .models import (
+    Job,
+    JobStatus,
+    Operator,
+    ETLConfiguration,
+    SMILEMessage,
+    RequestCallbackJob,
+    NormalizerModel,
+    ValidatorModel,
+)
 from advanced_filters.admin import AdminAdvancedFiltersMixin
 from lib.admin import pretty_json
 from beagle_etl.tasks import process_smile_message
@@ -107,8 +116,21 @@ class RequestCallbackJobAdmin(ModelAdmin):
     list_display = ("created_date", "request_id", "recipe", "status")
 
 
+class NormalizerAdmin(ModelAdmin):
+    list_display = (
+        "condition",
+        "normalizer",
+    )
+
+
+class ValidatorAdmin(ModelAdmin):
+    list_display = ("name",)
+
+
 admin.site.register(Job, JobAdmin)
 admin.site.register(Operator, OperatorAdmin)
 admin.site.register(ETLConfiguration, AssayAdmin)
 admin.site.register(SMILEMessage, SMILEMessagesAdmin)
 admin.site.register(RequestCallbackJob, RequestCallbackJobAdmin)
+admin.site.register(NormalizerModel, NormalizerAdmin)
+admin.site.register(ValidatorModel, ValidatorAdmin)
