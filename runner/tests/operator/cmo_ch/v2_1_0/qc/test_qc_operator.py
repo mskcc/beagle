c3cd016c74459725934dd64330babeb23e94c6df
buehlere <buehlere77@gmail.com>
2022-07-07 14:22:50 -0400

    Update test_qc_operator.py
    :heavy_check_mark: update to anonymous request id

--- runner/tests/operator/cmo_ch/v2_1_0/qc/test_qc_operator.py
+++ runner/tests/operator/cmo_ch/v2_1_0/qc/test_qc_operator.py
@@ -39,7 +39,7 @@ class TestAccessQCOperator(TestCase):
 
         # This operator needs to write a temp file, so need to override this env var
         settings.BEAGLE_SHARED_TMPDIR = "/tmp"
-        request_id = "05500_FH"
+        request_id = "A-000000"
 
         operator_model = Operator.objects.get(id=17)
         operator = OperatorFactory.get_by_model(operator_model, request_id=request_id)
