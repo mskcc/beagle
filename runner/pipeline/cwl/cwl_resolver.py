3a658af49ddff517619be697c741e3b977fedbe5
Allan Bolipata <allan.bolipata@gmail.com>
2022-07-14 17:08:46 -0400

    Adding cleanup back since it didn't do anything different - looks like git isn't cloning

--- runner/pipeline/cwl/cwl_resolver.py
+++ runner/pipeline/cwl/cwl_resolver.py
@@ -21,6 +21,7 @@ class CWLResolver(PipelineResolver):
             "--json"], stdout=out)
         with open(output_name) as f:
             pipeline = json.load(f)
+        self._cleanup(location)
         return pipeline
 
     def create_file(self):
