54a25b24e742932845d4a0ae6c6b6b93b715a74e
buehlere <buehlere77@gmail.com>
2022-07-14 10:43:24 -0400

    Update nucleo_qc_operator.py
    test for failing auto-tests

--- runner/operator/cmo_ch/v2_1_0/qc/nucleo_qc_operator.py
+++ runner/operator/cmo_ch/v2_1_0/qc/nucleo_qc_operator.py
@@ -75,6 +75,9 @@ class CMOCHNucleoOperatorQC(Operator):
 
     def get_nucleo_outputs(self):
         # Use most recent set of runs that completed successfully
+        print('TEST START HERE----------------------------------------------------')
+        print(self.request_id)
+        print(RunStatus.COMPLETED)
         most_recent_runs_for_request = (
             Run.objects.filter(
                 app__name="cmo-ch nucleo 2",
