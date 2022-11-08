88b7054d45e5c8b02be58794e1ce43d444f7a33a
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2022-11-08 13:01:45 -0500

    Copy files to other file_group

--- file_system/views/file_view.py
+++ file_system/views/file_view.py
@@ -1,6 +1,4 @@
-import uuid
-from distutils.util import strtobool
-from django.db.models import Prefetch, Count
+from django.conf import settings
 from django.db import transaction, IntegrityError
 from rest_framework import mixins
 from rest_framework import status
@@ -17,6 +15,7 @@ from file_system.serializers import (
     FileSerializer,
     FileQuerySerializer,
     BatchPatchFileSerializer,
+    CopyFilesSerializer
 )
 from drf_yasg.utils import swagger_auto_schema
 from beagle.pagination import time_filter
@@ -247,3 +246,32 @@ class BatchPatchFiles(GenericAPIView):
 
         success_message = "Successfully updated {} files".format(file_count)
         return Response(success_message, status=status.HTTP_200_OK)
+
+
+class CopyFilesView(GenericAPIView):
+
+    def _copy_files(self, files, file_group):
+        for f in files:
+            new_file = f.file
+            new_file.pk = None
+            new_file.file_group_id = file_group
+            new_file.save()
+            f.pk = None
+            f.file = new_file
+            f.save()
+
+    def post(self, request):
+        serializer = CopyFilesSerializer(data=request.data)
+        if serializer.is_valid():
+            request_id = serializer.data.get('request_id')
+            primary_id = serializer.data.get('primary_id')
+            file_group_from = serializer.data['file_group_from']
+            file_group_to = serializer.data['file_group_to']
+            if primary_id:
+                files = FileRepository.filter(metadata={settings.SAMPLE_ID_METADATA_KEY: primary_id},
+                                              file_group=file_group_from)
+            elif request_id:
+                files = FileRepository.filter(metadata={settings.REQUEST_ID_METADATA_KEY: request_id},
+                                              file_group=file_group_from)
+            self._copy_files(files, file_group_to)
+        return Response(status=status.HTTP_200_OK)
