9ac69d5831a07424462ec006ae8ad5ace755d693
Nikhil Kumar <nikhilkumar516@gmail.com>
2022-11-22 13:52:37 -0500

    Added black formatting

--- runner/migrations/0053_auto_20221122_1348.py
+++ runner/migrations/0053_auto_20221122_1348.py
@@ -7,18 +7,39 @@ import runner.models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('runner', '0052_remove_run_log_directory'),
+        ("runner", "0052_remove_run_log_directory"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='operatorrun',
-            name='status',
-            field=models.IntegerField(choices=[(0, 'CREATING'), (1, 'READY'), (2, 'RUNNING'), (3, 'FAILED'), (4, 'COMPLETED'), (5, 'TERMINATED')], db_index=True, default=runner.models.RunStatus(0)),
+            model_name="operatorrun",
+            name="status",
+            field=models.IntegerField(
+                choices=[
+                    (0, "CREATING"),
+                    (1, "READY"),
+                    (2, "RUNNING"),
+                    (3, "FAILED"),
+                    (4, "COMPLETED"),
+                    (5, "TERMINATED"),
+                ],
+                db_index=True,
+                default=runner.models.RunStatus(0),
+            ),
         ),
         migrations.AlterField(
-            model_name='run',
-            name='status',
-            field=models.IntegerField(choices=[(0, 'CREATING'), (1, 'READY'), (2, 'RUNNING'), (3, 'FAILED'), (4, 'COMPLETED'), (5, 'TERMINATED')], db_index=True),
+            model_name="run",
+            name="status",
+            field=models.IntegerField(
+                choices=[
+                    (0, "CREATING"),
+                    (1, "READY"),
+                    (2, "RUNNING"),
+                    (3, "FAILED"),
+                    (4, "COMPLETED"),
+                    (5, "TERMINATED"),
+                ],
+                db_index=True,
+            ),
         ),
     ]
