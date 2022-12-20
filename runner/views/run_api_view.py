61a3bb6523e31ea9b20f83294d2327dbd47780c2
Nikhil Kumar <nikhilkumar516@gmail.com>
2022-12-19 19:50:40 -0500

    Revert "Allow restart with jobs still running"
    This reverts commit 2d868c25b43746255ebb9670f9fe1e512e480f57.

--- runner/views/run_api_view.py
+++ runner/views/run_api_view.py
@@ -257,15 +257,13 @@ class RunApiRestartViewSet(GenericAPIView):
         if not o:
             return Response("Operator does not exist", status=status.HTTP_400_BAD_REQUEST)
 
-        runs_in_progress = len(o.runs.exclude(
-            status__in=[RunStatus.COMPLETED, RunStatus.FAILED, RunStatus.TERMINATED]))
-
+        runs_in_progress = len(o.runs.exclude(status__in=[RunStatus.COMPLETED, RunStatus.FAILED, RunStatus.TERMINATED]))
         if runs_in_progress:
-            job_group_id = runs_in_progress[0].job_group_id
-            terminate_job_task.delay(job_group_id, runs_in_progress)
+            return Response(
+                "There are runs still in progress, please terminate them to restart", status=status.HTTP_400_BAD_REQUEST
+            )
 
-        runs_to_restart = o.runs.exclude(status__in=RunStatus.COMPLETED)
-        runs_to_restart = runs_to_restart + runs_in_progress
+        runs_to_restart = o.runs.exclude(status=RunStatus.COMPLETED)
 
         if not runs_to_restart:
             return Response("There are no runs to restart", status=status.HTTP_400_BAD_REQUEST)
