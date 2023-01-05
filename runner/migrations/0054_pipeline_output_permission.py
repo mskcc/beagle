998ee4382eacf26ec25cbd7c5a0c6a2fd03f479d
Nikhil Kumar <nikhilkumar516@gmail.com>
2023-01-05 15:31:53 -0500

    Added black formatting

--- runner/migrations/0054_pipeline_output_permission.py
+++ runner/migrations/0054_pipeline_output_permission.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('runner', '0053_auto_20221122_1348'),
+        ("runner", "0053_auto_20221122_1348"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='pipeline',
-            name='output_permission',
+            model_name="pipeline",
+            name="output_permission",
             field=models.IntegerField(blank=True, null=True),
         ),
     ]
