0d8c6cc492fe855046a231cf433ac52d69fb8be2
Nikhil Kumar <nikhilkumar516@gmail.com>
2023-03-14 07:08:10 -0400

    Added operator changes to work with study obj

--- runner/operator/aion/v1_0_0/aion_operator.py
+++ runner/operator/aion/v1_0_0/aion_operator.py
@@ -15,21 +15,26 @@ LOGGER = logging.getLogger(__name__)
 
 
 class AionOperator(Operator):
-    def get_jobs(self, lab_head_email):
+    def get_jobs(self, study_obj):
         """
         From self, retrieve relevant run IDs, build the input JSON for
         the pipeline, and then submit them as jobs through the
         RunCreator
         """
-        run_ids = self.get_helix_filter_run_ids(lab_head_email)
+        run_ids = study_obj.run_ids
+        study_id = study_obj.study_id
+        project_prefixes = study_obj.proj_prefixes
+        samples = study_obj.samples
+        # How should we separate dmp samples from regular samples in the study obj?
+
+        dmp_samples = []
+
         number_of_runs = len(run_ids)
-        name = "AION merging %i runs for lab head email %s" % (number_of_runs, lab_head_email)
+        name = "AION merging %i runs for lab head email %s" % (number_of_runs, study_obj.lab_head_email)
 
         app = self.get_pipeline_id()
-        pipeline = Pipeline.objects.get(id=app)
-        pipeline_version = pipeline.version
-        input_json = self.build_input_json(run_ids)
-        tags = {"study_id": input_json["study_id"], "num_runs_merged": len(run_ids)}
+        input_json = self.build_input_json(run_ids, project_prefixes, study_id, dmp_samples)
+        tags = {"study_id": study_id, "num_runs_merged": len(run_ids)}
         print(input_json)
 
         aion_outputs_job_data = {"app": app, "inputs": input_json, "name": name, "tags": tags}
@@ -38,37 +43,56 @@ class AionOperator(Operator):
 
         return aion_outputs_job
 
-    def build_input_json(self, run_ids):
+    def build_input_json(self, run_ids, project_prefixes, study_id, dmp_samples):
         #    run_ids = self.run_ids
-        project_prefixes = set()
-        directories = set()
-        lab_heads = set()
-        argos_run_ids = set()
-
-        for run_id in run_ids:
-            run = Run.objects.get(id=run_id)
-            argos_run_ids = run.tags["run_ids"]
-            project_prefix = run.tags["project_prefix"]
-            if project_prefix:
-                project_prefixes.add(project_prefix)
-            run_portal_dir = os.path.join(run.output_directory, "portal")
-            if os.path.isdir(run_portal_dir):
-                directories.add(run_portal_dir)
         merge_date = datetime.datetime.now().strftime("%Y_%m_%d")
-        lab_head_email = self.get_lab_head(argos_run_ids)
+        directories = set()
         input_json = dict()
-        lab_head_id = lab_head_email.split("@")[0]
-        study_id = "set_%s" % lab_head_id
+
         input_json["project_description"] = "[%s] Includes samples received at IGO for Project ID(s): %s" % (
             merge_date,
             ", ".join(sorted(project_prefixes)),
         )
         input_json["study_id"] = study_id
         input_json["project_title"] = "CMO Merged Study for Principal Investigator (%s)" % study_id
+
+        for run_id in run_ids:
+            run = Run.objects.get(id=run_id)
+            if 'helix_filters' in run.app.name:
+                run_portal_dir = os.path.join(run.output_directory, "portal")
+                if os.path.isdir(run_portal_dir):
+                    directories.add(run_portal_dir)
+            if 'ultron' in run.app.name:
+                output_dir = run.output_directory
+                if os.path.isdir(output_dir):
+                    directories.add(output_dir)
         input_json["directories"] = list()
+        input_json["sample_data_clinical_files"] = [self.create_data_clinical_file(run_ids, dmp_samples)]
         for portal_directory in directories:
             input_json["directories"].append({"class": "Directory", "path": portal_directory})
         return input_json
+
+    def create_data_clinical_file(self, run_id_list, dmp_samples):
+        files = list()
+        pipeline_names = set()
+        pipeline_githubs = set()
+        pipeline_versions = set()
+        for run_id in run_id_list:
+            argos_run = Run.objects.get(id=run_id)
+            pipeline = argos_run.app
+            pipeline_names.add(pipeline.name)
+            pipeline_githubs.add(pipeline.github)
+            pipeline_versions.add(pipeline.version)
+            files = files + get_files_from_run(argos_run)
+        data_clinical_content = generate_sample_data_content(
+            files,
+            pipeline_name=",".join(pipeline_names),
+            pipeline_github=",".join(pipeline_githubs),
+            pipeline_version=",".join(pipeline_versions),
+            dmp_samples=dmp_samples,
+        )
+        data_clinical_content = data_clinical_content.strip()
+        return {"class": "File", "basename": "sample_data_clinical.txt", "contents": data_clinical_content}
 
     def get_lab_head(self, argos_run_ids):
         lab_head_emails = set()
