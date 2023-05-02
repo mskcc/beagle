430e7c12bbda02adacc48c99119602555bb18474
Ivkovic <Sinisa Ivkovic>
2023-05-02 10:44:31 -0400

    add contact email as config

--- notifier/events/voyager_is_processing_whole_request_event.py
+++ notifier/events/voyager_is_processing_whole_request_event.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from notifier.event_handler.event import Event
 
 
@@ -53,6 +54,6 @@ Samples Paired with Pooled normal: {self.pooled_normal_cnt}<br>
 Thank you,<br>
 Nicholas D. Socci<br>
 Director, Bioinformatics Core<br>
-zzPDL_CMO_Pipeline_Support@mskcc.org<br>
+{settings.CONTACT_EMAIL}<br>
 """
         return body
