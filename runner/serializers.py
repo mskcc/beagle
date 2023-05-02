1a0983c6eb343750e609f9a463394f4d0f1b7db5
Ivkovic <Sinisa Ivkovic>
2023-05-02 11:06:23 -0400

    Add file_group in pairs and request serializers

--- runner/serializers.py
+++ runner/serializers.py
@@ -270,6 +270,7 @@ class RequestIdsOperatorSerializer(serializers.Serializer):
     pipeline = serializers.CharField(max_length=30, allow_null=False, allow_blank=False)
     pipeline_version = serializers.CharField(max_length=30, allow_null=True, allow_blank=True)
     job_group_id = serializers.UUIDField(required=False)
+    file_group_id = serializers.UUIDField(required=False)
     for_each = serializers.BooleanField(required=False, default=True)
 
 
@@ -288,6 +289,7 @@ class PairOperatorSerializer(serializers.Serializer):
     name = serializers.CharField(allow_blank=False, allow_null=False)
     output_directory_prefix = serializers.CharField(max_length=50, allow_blank=True, allow_null=True)
     job_group_id = serializers.UUIDField(required=False)
+    file_group_id = serializers.UUIDField(required=False)
     request_id = serializers.CharField()
     labHeadName = serializers.CharField(max_length=30, required=False, allow_blank=True, allow_null=True)
     investigatorName = serializers.CharField(max_length=30, required=False, allow_blank=True, allow_null=True)
