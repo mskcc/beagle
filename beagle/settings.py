e1f9b41b8eadfa6c272699288413baf3b403c666
Ivkovic <Sinisa Ivkovic>
2023-05-04 10:08:29 -0400

    Fix getting email for runs

--- beagle/settings.py
+++ beagle/settings.py
@@ -467,4 +467,4 @@ DEFAULT_LOG_PATH = os.environ.get("BEAGLE_DEFAULT_LOG_PATH", "/tmp/%s")
 
 APP_CACHE = os.environ.get("BEAGLE_APP_CACHE", "/tmp")
 
-CONTACT_EMAIL = os.environ("EVENTS_CONTACT_EMAIL", "")
+CONTACT_EMAIL = os.environ.get("EVENTS_CONTACT_EMAIL", "")
