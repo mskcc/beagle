143027b7350dc4ecf6449e5eeb6833ced7d1b9b6
Allan Bolipata <allan.bolipata@gmail.com>
2023-05-22 17:07:37 -0400

    Add back test

--- file_system/test_manifest_view.py
+++ file_system/test_manifest_view.py
@@ -37,14 +37,14 @@ class TestManifest(TestCase):
         settings.DMP_BAM_FILE_GROUP = "6b3bf1ec-4cc0-441c-8fd7-0c104091cd58"
         self.response_csv = 'igoRequestId,primaryId,cmoPatientId,dmpPatientId,dmpImpactSamples,dmpAccessSamples,baitSet,libraryVolume,investigatorSampleId,preservation,species,libraryConcentrationNgul,tissueLocation,sampleClass,sex,cfDNA2dBarcode,sampleOrigin,tubeId,tumorOrNormal,captureConcentrationNm,oncotreeCode,dnaInputNg,collectionYear,captureInputNg\r\n12345_A,12345_A_1,C-ALLANT2,P-0000002,P-0000005-T01-IM6;P-0000004-T01-IM6,,MSK-ACCESS-v1_0-probesAllwFP,25.0,P-REDACT,,,69.0,,,M,,Whole Blood,,Normal,14.49275362,,200.0,,999.99999978\r\n12345_A,12345_A_3,C-ALLANT,P-0000001,P-0000002-T01-IM6;P-0000001-T01-IM6,,MSK-ACCESS-v1_0-probesAllwFP,25.0,P-REDACT,EDTA-Streck,,102.5,,Blood,M,8042889270,Whole Blood,,Normal,9.756097561,,200.0,,1000.0000000025001\r\n12345_A,12345_A_2,C-ALLANT2,P-0000002,P-0000005-T01-IM6;P-0000004-T01-IM6,,MSK-ACCESS-v1_0-probesAllwFP,25.0,P-REDACT,,,74.5,,,M,,Whole Blood,,Normal,13.42281879,,200.0,,999.999999855\r\n'
 
-#    def test_manifest_endpoint(self):
-#        """ """
-#        # make a GET request to the API endpoint
-#        response = self.client.get("/v0/fs/manifest/?request_id=12345_A")
-#        # check that the response status code is 200
-#        self.assertEqual(response.status_code, 200)
-#        # check that the response contains the correct data
-#        response_data = response.content.decode()
-#        self.assertEqual(response.status_code, 200)
-#        self.maxDiff = None
-#        self.assertEqual(response_data, self.response_csv)
+    def test_manifest_endpoint(self):
+        """ """
+        # make a GET request to the API endpoint
+        response = self.client.get("/v0/fs/manifest/?request_id=12345_A")
+        # check that the response status code is 200
+        self.assertEqual(response.status_code, 200)
+        # check that the response contains the correct data
+        response_data = response.content.decode()
+        self.assertEqual(response.status_code, 200)
+        self.maxDiff = None
+        self.assertEqual(response_data, self.response_csv)
