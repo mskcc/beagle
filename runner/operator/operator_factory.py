46000408f27e3bc6b0244ed2c60ded1dcd406b17
Allan Bolipata <allan.bolipata@gmail.com>
2023-05-22 15:14:38 -0400

    Add pipeline retrieval error checking in OperatorFactory

--- runner/operator/operator_factory.py
+++ runner/operator/operator_factory.py
@@ -1,5 +1,6 @@
 import importlib
 from beagle_etl.models import Operator
+from runner.models import Pipeline
 
 
 class OperatorFactory(object):
@@ -22,4 +23,17 @@ class OperatorFactory(object):
         mod_name, func_name = model.class_name.rsplit(".", 1)
         mod = importlib.import_module(mod_name)
         operator_class = getattr(mod, func_name)
+
+        from pprint import pprint
+        for operator_pipeline in model.pipeline_set.all():
+            pprint("pipeline_version is " + operator_pipeline.version)
+        if "pipeline_version" in kwargs:
+            pipeline_version = kwargs.get("pipeline_version")
+            pipeline_exists = any([
+                p_version_bool in operator_pipeline.version for operator_pipeline in model.pipeline_set.all()
+            ])
+            if not pipeline_exists:
+                raise PipelineNotFoundError(
+                    "Version {pipeline_version} is not found for Pipelines associated with Operator {model_name}"
+                .format(pipeline_version=pipeline_version, model_name=mod_name))
         return operator_class(model, **kwargs)
