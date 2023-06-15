e24d5290210515c5df7121403fccc44bf7b0da5c
buehlere <buehlere@mskcc.org>
2023-06-15 10:59:30 -0400

    fix test and apply black formatting

--- runner/tests/operator/access/manifest/test_access_manifest_operator.py
+++ runner/tests/operator/access/manifest/test_access_manifest_operator.py
@@ -33,8 +33,9 @@ class TestAcessManifestOperator(TestCase):
     # test db
     fixtures = [os.path.join(ROOT_DIR, f) for f in COMMON_FIXTURES]
     # variables to help check operator output
-    expected_csv_content = 'igoRequestId,primaryId,cmoPatientId,dmpPatientId,dmpImpactSamples,dmpAccessSamples,baitSet,libraryVolume,investigatorSampleId,preservation,species,libraryConcentrationNgul,tissueLocation,sampleClass,sex,cfDNA2dBarcode,sampleOrigin,tubeId,tumorOrNormal,captureConcentrationNm,oncotreeCode,dnaInputNg,collectionYear,captureInputNg\n13893_B,13893_B_1,ALLANT2,P-0000002,P-0000005-T01-IM6;P-0000004-T01-IM6,,MSK-ACCESS-v1_0-probesAllwFP,25.0,P-1234567-N00-XS1,EDTA-Streck,,69.0,,Blood,M,8042889270,Whole Blood,,Normal,14.49275362,,200.0,,999.99999978\n13893_B,13893_B_3,ALLANT,P-0000001,P-0000002-T01-IM6;P-0000001-T01-IM6,,MSK-ACCESS-v1_0-probesAllwFP,25.0,P-1234567-N00-XS1,EDTA-Streck,,102.5,,Blood,M,8042889270,Whole Blood,,Normal,9.756097561,,200.0,,1000.0000000025001\n13893_B,13893_B_2,ALLANT3,,,,MSK-ACCESS-v1_0-probesAllwFP,25.0,P-1234567-N00-XS1,EDTA-Streck,,74.5,,Blood,M,8042889270,Whole Blood,,Normal,13.42281879,,200.0,,999.999999855\n""\n'
-    file_path_pattern = "/tmp/13893_B/*/manifest.csv"
+    expected_csv_content = [
+        'igoRequestId,primaryId,cmoPatientId,dmpPatientId,dmpImpactSamples,dmpAccessSamples,baitSet,libraryVolume,investigatorSampleId,preservation,species,libraryConcentrationNgul,tissueLocation,sampleClass,sex,cfDNA2dBarcode,sampleOrigin,tubeId,tumorOrNormal,captureConcentrationNm,oncotreeCode,dnaInputNg,collectionYear,captureInputNg\n13893_B,13893_B_1,ALLANT2,P-0000002,P-0000005-T01-IM6;P-0000004-T01-IM6,,MSK-ACCESS-v1_0-probesAllwFP,25.0,P-1234567-N00-XS1,EDTA-Streck,,69.0,,Blood,M,8042889270,Whole Blood,,Normal,14.49275362,,200.0,,999.99999978\n13893_B,13893_B_3,ALLANT,P-0000001,P-0000002-T01-IM6;P-0000001-T01-IM6,,MSK-ACCESS-v1_0-probesAllwFP,25.0,P-1234567-N00-XS1,EDTA-Streck,,102.5,,Blood,M,8042889270,Whole Blood,,Normal,9.756097561,,200.0,,1000.0000000025001\n13893_B,13893_B_2,ALLANT3,,,,MSK-ACCESS-v1_0-probesAllwFP,25.0,P-1234567-N00-XS1,EDTA-Streck,,74.5,,Blood,M,8042889270,Whole Blood,,Normal,13.42281879,,200.0,,999.999999855\n""\n'
+    ]
 
     def test_access_manifest_operator(self):
         """
@@ -45,7 +46,6 @@ class TestAcessManifestOperator(TestCase):
         request_id = "13893_B"
         operator_model = Operator.objects.get(id=20)
         operator = OperatorFactory.get_by_model(operator_model, request_id=request_id)
-        operator.OUTPUT_DIR = "/tmp"  # overide directory defaults for testing
         self.assertEqual(operator.get_pipeline_id(), "0d6c2863-52a2-4c5b-ad1b-8664ee74b7b7")
         self.assertEqual(str(operator.model), "AccessManifestOperator_2_1_0")
         self.assertEqual(operator.request_id, request_id)
@@ -53,21 +53,13 @@ class TestAcessManifestOperator(TestCase):
         # Get Jobs
         jobs = operator.get_jobs()
         self.assertEqual(len(jobs) > 0, True)
-        # Check if the file exists, use glob since we are unsure of what the date will be
-        file = glob.glob(self.file_path_pattern)
-        self.assertEqual(len(file) == 1, True)
-        file_name = file[0]
-        # Check contents
-        with open(file_name, "r") as file:
-            csv_string = file.read()
-        self.assertEqual(csv_string, self.expected_csv_content)
-        # Check if file can be queried
-        file_query = File.objects.filter(file_group__slug="access_manifests", path=file_name)
-        # Check Query data
-        self.assertEqual(len(file_query) == 1, True)
-        file_query_object = file_query[0]
-        self.assertEqual(file_query_object.path, file_name)
-
-    def tearDown(self):
-        # clean up tmp directory
-        shutil.rmtree("/tmp/13893_B")
+        for i, job in enumerate(jobs):
+            # check job json is correct
+            self.assertEqual(job.is_valid(), True)
+            input_json = job.inputs
+            self.assertEqual(len(input_json["manifest_data"]), 2)
+            # Check contents
+            manifest_path = input_json["manifest_data"]["location"].replace("juno:", "")
+            with open(manifest_path, "r") as file:
+                csv_string = file.read()
+            self.assertEqual(csv_string, self.expected_csv_content[i])
