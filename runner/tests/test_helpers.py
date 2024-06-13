01e1a5e5edd4680bdd9925c82677e8d86be4da26
Allan Bolipata <allan.bolipata@gmail.com>
2024-06-13 13:49:14 -0400

    Update black formatting

--- runner/tests/test_helpers.py
+++ runner/tests/test_helpers.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from runner.operator.helper import format_sample_name
 
+
 class TestHelper(TestCase):
     fixtures = {
         "file_system.filegroup.json",
@@ -23,7 +24,9 @@ class TestHelper(TestCase):
         sample_name_old = "C-12345H-T"
         sample_name_random = "ABCDEFGHIJKLMN"
 
-        self.assertEqual(format_sample_name(sample_name_malformed, "Primary"), "sampleNameMalformed") 
-        self.assertEqual(format_sample_name(sample_name_random, "Primary"), "s_" + sample_name_random) # should be prefixed with 's_'
-        self.assertEqual(format_sample_name(sample_name_ci_tag, "Primary"), sample_name_ci_tag) # should be unchanged
-        self.assertEqual(format_sample_name(sample_name_old, "Primary"), sample_name_ci_tag) # should be converted
+        self.assertEqual(format_sample_name(sample_name_malformed, "Primary"), "sampleNameMalformed")
+        self.assertEqual(
+            format_sample_name(sample_name_random, "Primary"), "s_" + sample_name_random
+        )  # should be prefixed with 's_'
+        self.assertEqual(format_sample_name(sample_name_ci_tag, "Primary"), sample_name_ci_tag)  # should be unchanged
+        self.assertEqual(format_sample_name(sample_name_old, "Primary"), sample_name_ci_tag)  # should be converted
