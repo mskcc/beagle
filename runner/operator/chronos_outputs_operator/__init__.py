d11f67b2ef1c374030232ddbdfa7eaab97a3f0e1
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2024-07-17 18:29:12 -0400

    Add registering files with metadata after Tempo complete

--- runner/operator/chronos_outputs_operator/__init__.py
+++ runner/operator/chronos_outputs_operator/__init__.py
@@ -2,10 +2,14 @@ import io
 import os
 import logging
 import subprocess
+
+from django.conf import settings
+
+from file_system.helper.checksum import sha1
 from runner.models import Pipeline
 from runner.models import Run
 from runner.operator.operator import Operator
-
+from runner.run.processors.file_processor import FileProcessor
 
 LOGGER = logging.getLogger(__name__)
 
@@ -19,12 +23,13 @@ class ChronosCopyOutputOperator(Operator):
         for run_id in self.run_ids:
             LOGGER.info(f"Copy outputs for {run_id} for {destination_directory}")
             run = Run.objects.get(id=run_id)
-            self.copy_bams(run, destination_directory)
+            metadata = run.tags
+            self.copy_bams(run, destination_directory, metadata)
             self.append_trace(run, destination_directory)
             self.append_bam_outputs(run, destination_directory)
         return []
 
-    def recursive_copy(self, src, dst):
+    def recursive_copy(self, src, dst, metadata={}):
         for item in os.listdir(src):
             if os.path.isdir(os.path.join(src, item)):
                 if not os.path.exists(os.path.join(dst, item)):
@@ -35,17 +40,24 @@ class ChronosCopyOutputOperator(Operator):
                 self.recursive_copy(os.path.join(src, item), os.path.join(dst, item))
             else:
                 LOGGER.info(f"Copying file {os.path.join(src, item)}")
-                self._copy(os.path.join(src, item), os.path.join(dst, item))
+                self._copy(os.path.join(src, item), os.path.join(dst, item), metadata)
 
-    def copy_bams(self, run, destination_directory):
+    def copy_bams(self, run, destination_directory, metadata={}):
         source_bam_directory = os.path.join(run.output_directory, "bams")
         destination_bam_directory = os.path.join(destination_directory, "bams")
-        self.recursive_copy(source_bam_directory, destination_bam_directory)
+        self.recursive_copy(source_bam_directory, destination_bam_directory, metadata)
 
-    def _copy(self, src, dst):
+    def _copy(self, src, dst, metadata):
         ret = subprocess.call(f"cp {src} {dst}", shell=True)
         if ret != 0:
             LOGGER.error(f"Failed to copy {src}")
+        else:
+            path = f"juno://{dst}"
+            size = os.path.getsize(dst)
+            checksum = sha1(dst)
+            request_id = metadata[settings.REQUEST_ID_METADATA_KEY]
+            sample_id = metadata[settings.SAMPLE_ID_METADATA_KEY]
+            FileProcessor.create_file_obj(path, size, checksum, self.file_group, metadata, request_id, [sample_id])
 
     def append_trace(self, run, destination_directory):
         trace_file_path = os.path.join(run.output_directory, "trace.txt")
