bc2715914601b7da1dd55fb2d8f7efbba155abc6
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2024-07-26 07:20:15 -0400

    Missing files implementation

--- file_system/tasks.py
+++ file_system/tasks.py
@@ -1,6 +1,11 @@
 import logging
+import os.path
+from time import sleep
+from django.apps import apps
 from celery import shared_task
+from django.conf import settings
 from notifier.models import JobGroupNotifier
+
 
 logger = logging.getLogger(__name__)
 
@@ -20,3 +25,28 @@ def populate_job_group_notifier_metadata(request_id, pi, investigator, assay):
         job_group_notifier.investigator = investigator
         job_group_notifier.assay = assay
         job_group_notifier.save()
+
+
+@shared_task
+def check_for_missing_files():
+    file_groups = settings.CHECK_FILE_GROUPS
+    File = apps.get_model(app_label="file_system", model_name="File")
+    # Check for missing files
+    for fg in file_groups:
+        files = File.objects.filter(file_group=fg).all()
+        for f in files:
+            if not os.path.exists(path=f.path):
+                f.set_missing()
+            else:
+                if f.missing:
+                    f.set_not_missing()
+            sleep(5)
+    generate_report_of_missing_files.delay()
+
+
+@shared_task()
+def generate_report_of_missing_files():
+    file_groups = settings.CHECK_FILE_GROUPS
+    File = apps.get_model(app_label="file_system", model_name="File")
+    for fg in file_groups:
+        files = File.objects.filter(file_group=fg, missing=True).all()
