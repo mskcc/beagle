ae779efdfeca64c8cd27d69995861701e71b3f3b
buehlere <buehlere@mskcc.org>
2024-10-21 14:25:59 -0400

    linting and test update

--- runner/migrations/0062_pipeline_nfcore_template.py
+++ runner/migrations/0062_pipeline_nfcore_template.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('runner', '0061_auto_20240820_1837'),
+        ("runner", "0061_auto_20240820_1837"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='pipeline',
-            name='nfcore_template',
+            model_name="pipeline",
+            name="nfcore_template",
             field=models.BooleanField(default=False),
         ),
     ]
