1b898c409dad6a657be3ea0a4154069260fbeaa7
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2024-10-30 09:48:02 -0400

    Fix import

--- runner/operator/argos_operator/v2_0_0/argos_operator.py
+++ runner/operator/argos_operator/v2_0_0/argos_operator.py
@@ -13,8 +13,7 @@ from notifier.events import (
     OperatorRequestEvent,
     CantDoEvent,
     SetLabelEvent,
-    LocalStoreAttachmentsEvent,
-    AddAttachmentLinksToDescriptionEvent,
+    LocalStoreAttachmentsEvent
 )
 from notifier.tasks import send_notification
 from notifier.helper import generate_sample_data_content
