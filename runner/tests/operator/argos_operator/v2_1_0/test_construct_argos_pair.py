d0a825bb12e5f73bbc80c4fb65383916b150bc6e
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2025-01-14 13:31:48 -0500

    ArgosOperator 2.1.0

--- runner/tests/operator/argos_operator/v2_1_0/test_construct_argos_pair.py
+++ runner/tests/operator/argos_operator/v2_1_0/test_construct_argos_pair.py
@@ -7,8 +7,8 @@ from django.test import TestCase
 from django.conf import settings
 from django.core.management import call_command
 from file_system.models import File
-from runner.operator.argos_operator.v1_2_0.bin.make_sample import build_sample
-from runner.operator.argos_operator.v1_2_0.construct_argos_pair import construct_argos_jobs
+from runner.operator.argos_operator.v2_1_0.bin.make_sample import build_sample
+from runner.operator.argos_operator.v2_1_0.construct_argos_pair import construct_argos_jobs
 
 
 class TestConstructPair(TestCase):
@@ -59,7 +59,48 @@ class TestConstructPair(TestCase):
 
         argos_inputs, error_samples = construct_argos_jobs(samples)
         expected_inputs = json.load(
-            open(os.path.join(settings.TEST_FIXTURE_DIR, "10075_D_single_TN_pair.argos.input.json"))
+            open(os.path.join(settings.TEST_FIXTURE_DIR, "10075_D_single_TN_pair.argos_1_7_0.input.json"))
         )
+
         print("Running test_construct_argos_jobs1")
+        self.assertTrue(argos_inputs == expected_inputs)
+
+    def test_construct_argos_jobs_pdx(self):
+        test_files_fixture = os.path.join(settings.TEST_FIXTURE_DIR, "08944_B.fixtures.json")
+        call_command("loaddata", test_files_fixture, verbosity=0)
+
+        request_id_metadata_key = settings.REQUEST_ID_METADATA_KEY
+        igo_complete_metadata_key = settings.IGO_COMPLETE_METADATA_KEY
+
+        files = File.objects.filter(
+            **{
+                "filemetadata__metadata__{}".format(request_id_metadata_key): "08944_B",
+                "filemetadata__metadata__{}".format(igo_complete_metadata_key): True,
+            }
+        ).all()
+        data = list()
+        for file in files:
+            sample = dict()
+            sample["id"] = file.id
+            sample["path"] = file.path
+            sample["file_name"] = file.file_name
+            sample["metadata"] = file.filemetadata_set.first().metadata
+            data.append(sample)
+        samples = list()
+
+        igo_id_group = dict()
+        for sample in data:
+            igo_id = sample["metadata"][settings.SAMPLE_ID_METADATA_KEY]
+            if igo_id not in igo_id_group:
+                igo_id_group[igo_id] = list()
+            igo_id_group[igo_id].append(sample)
+
+        for igo_id in igo_id_group:
+            samples.append(build_sample(igo_id_group[igo_id]))
+
+        argos_inputs, error_samples = construct_argos_jobs(samples)
+        expected_inputs = json.load(
+            open(os.path.join(settings.TEST_FIXTURE_DIR, "08944_B.argos.input.json"))
+        )
+        print("Running test_construct_argos_jobs_pdx")
         self.assertTrue(argos_inputs == expected_inputs)
