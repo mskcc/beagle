02b3eae16eb7a4b5508d2c51ffa6281ee6edd66e
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2025-03-26 15:31:05 -0400

    Fix logging for log_directory

--- runner/tasks.py
+++ runner/tasks.py
@@ -534,7 +534,7 @@ def submit_job(run_id, output_directory=None, execution_id=None):
     if not output_directory:
         output_directory = os.path.join(run.app.output_directory, str(run_id))
     job = run1.dump_job(output_directory=output_directory)
-    logger.info(format_log("Log output directory {path}".format(path=run1.log_directory), obj=run1))
+    logger.info(format_log("Log output directory {path}".format(path=run1.run_obj.log_directory), obj=run1))
     logger.info(format_log("Job ready for submitting", obj=run1))
     if resume:
         url = urljoin(settings.RIDGEBACK_URL, "/v0/jobs/{id}/resume/".format(id=resume))
