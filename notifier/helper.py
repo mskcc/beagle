6a4cfa79e5f7ec112e764d1cf324dd1fe5f7791e
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2025-05-19 11:56:08 -0400

    genePanel normalization in sample_data

--- notifier/helper.py
+++ notifier/helper.py
@@ -75,5 +75,9 @@ def generate_sample_data_content_str(metadata, pipeline_name, pipeline_github, p
             metadata[settings.CMO_SAMPLE_NAME_METADATA_KEY], metadata[settings.SAMPLE_CLASS_METADATA_KEY]
         ),
     )
-    result = f"{sample_id}\t{metadata[settings.REQUEST_ID_METADATA_KEY]}\t{project_id}\t{metadata[settings.PATIENT_ID_METADATA_KEY]}\t{metadata['investigatorSampleId']}\t{MetadataValidator.clean_value(metadata[settings.CMO_SAMPLE_CLASS_METADATA_KEY])}\t{MetadataValidator.clean_value(metadata[settings.RECIPE_METADATA_KEY])}\t{MetadataValidator.clean_value(metadata[settings.ONCOTREE_METADATA_KEY])}\t{MetadataValidator.clean_value(metadata[settings.SAMPLE_CLASS_METADATA_KEY])}\t{MetadataValidator.clean_value(metadata['preservation'])}\t{MetadataValidator.clean_value(metadata['sex'])}\t{MetadataValidator.clean_value(metadata['tissueLocation'])}\t{metadata[settings.SAMPLE_ID_METADATA_KEY]}\t{MetadataValidator.clean_value(metadata['runMode'])}\t{pipeline_name}\t{pipeline_github}\t{pipeline_version}\n"
+    result = f"{sample_id}\t{metadata[settings.REQUEST_ID_METADATA_KEY]}\t{project_id}\t{metadata[settings.PATIENT_ID_METADATA_KEY]}\t{metadata['investigatorSampleId']}\t{MetadataValidator.clean_value(metadata[settings.CMO_SAMPLE_CLASS_METADATA_KEY])}\t{gene_panel_mapping(MetadataValidator.clean_value(metadata[settings.RECIPE_METADATA_KEY]), MetadataValidator.clean_value(metadata[settings.BAITSET_METADATA_KEY]))}\t{MetadataValidator.clean_value(metadata[settings.ONCOTREE_METADATA_KEY])}\t{MetadataValidator.clean_value(metadata[settings.SAMPLE_CLASS_METADATA_KEY])}\t{MetadataValidator.clean_value(metadata['preservation'])}\t{MetadataValidator.clean_value(metadata['sex'])}\t{MetadataValidator.clean_value(metadata['tissueLocation'])}\t{metadata[settings.SAMPLE_ID_METADATA_KEY]}\t{MetadataValidator.clean_value(metadata['runMode'])}\t{pipeline_name}\t{pipeline_github}\t{pipeline_version}\n"
     return result
+
+
+def gene_panel_mapping(gene_panel, bait_set):
+    return settings.GENE_PANEL_TABLE.get(gene_panel, {}).get(bait_set, "ERROR")
