bb381bcf54c04ff17749c1a1330f3a45acdb7055
Nikhil Kumar <nikhilkumar516@gmail.com>
2025-05-27 15:27:00 -0400

    Update argos operator from PR #1439

--- runner/operator/argos_operator/v2_3_0/argos_operator.py
+++ runner/operator/argos_operator/v2_3_0/argos_operator.py
@@ -486,12 +486,12 @@ Comments\tQC Report Type\tIGORecommendation\tInvestigator Decision\n
         jira_id = JobGroupNotifier.objects.get(id=self.job_group_notifier_id).jira_id
         result = dict()
         result["Sample Pairing"] = (
-            f"{settings.DELIVERY_FILE_SERVER}/project/{self.request_id}/jira/{jira_id}/sample_pairing.txt"
+            f"{settings.DELIVERY_FILE_SERVER}/project/{self.request_id}/jira/{jira_id}/preview/sample_pairing.txt"
         )
         result["Sample Mapping"] = (
-            f"{settings.DELIVERY_FILE_SERVER}/project/{self.request_id}/jira/{jira_id}/sample_mapping.txt"
+            f"{settings.DELIVERY_FILE_SERVER}/project/{self.request_id}/jira/{jira_id}/preview/sample_mapping.txt"
         )
         result["Sample Data Clinical"] = (
-            f"{settings.DELIVERY_FILE_SERVER}/project/{self.request_id}/jira/{jira_id}/sample_data_clinical.txt"
+            f"{settings.DELIVERY_FILE_SERVER}/project/{self.request_id}/jira/{jira_id}/preview/sample_data_clinical.txt"
         )
         return result
