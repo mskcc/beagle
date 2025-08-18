e6b22d933a86ecb601dca91b49502eac002f8a3d
buehlere <buehlere@mskcc.org>
2025-08-18 11:35:45 -0400

    tmp dir fix for container

--- runner/pipeline/pipeline_resolver.py
+++ runner/pipeline/pipeline_resolver.py
@@ -25,9 +25,11 @@ class PipelineResolver(object):
         return dirname
 
     def _dir_name(self):
-        dirname = "/tmp/" + str(uuid.uuid4())
+        base = os.environ.get("TMPDIR", "/tmp")
+        dirname = os.path.join(base, str(uuid.uuid4()))
         os.mkdir(dirname)
         return dirname
+
 
     def _extract_dirname_from_github_link(self):
         dirname = self.github.rsplit("/", 2)[1] if self.github.endswith("/") else self.github.rsplit("/", 1)[1]
