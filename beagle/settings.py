357be5b6b84e384408e608af0bec386086ba9012
Sinisa Ivkovic <sinisa.ivkovic@gmail.com>
2026-05-06 14:02:38 -0400

    Use whitenoise for static

--- beagle/settings.py
+++ beagle/settings.py
@@ -95,6 +95,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
