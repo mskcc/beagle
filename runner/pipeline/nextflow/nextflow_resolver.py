9b0233053a381097919ec54c70efc9c0b8836aff
buehlere <buehlere77@gmail.com>
2026-09-11 14:35:00 -0400

    Update nextflow_resolver.py

--- runner/pipeline/nextflow/nextflow_resolver.py
+++ runner/pipeline/nextflow/nextflow_resolver.py
@@ -16,7 +16,10 @@ class NextflowResolver(PipelineResolver):
             with open(os.path.join(location, "nextflow_schema.json"), "r") as f:
                 nextflow_schema = json.load(f)
                 inputs = self.schemas2template(nextflow_schema, location)
-                pipeline = {"inputs": inputs}
+                outputs = nextflow_schema.get("outputs") or [
+                    {"id": "outputs", "schema": {"type": {"items": "File", "type": "array"}}}
+                ]
+                pipeline = {"inputs": inputs, "outputs": outputs}
         else:
             with open(os.path.join(location, "inputs.template.json"), "r") as f:
                 pipeline = json.load(f)
